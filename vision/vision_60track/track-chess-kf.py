80deea8c39e2e5d7bbd9fc063f09c3f888cf16e4
BB <bb@b.om>
2017-03-29 07:24:58 +0200

    -

--- vision/vision_60track/track-chess-kf.py
+++ vision/vision_60track/track-chess-kf.py
@@ -1,27 +1,20 @@
-import cv2
 import sys; sys.path.append('../../tser/tser_kf')
+import cv2
+import util
 from kalman_3d import *
-from K import *
-
-def proj_board(im, xl, yl, z):
-    h,w = im.shape[:2]
-    for x in arange(xl-9, xl+9, 0.5):
-        for y in arange(yl-9, yl+9, 0.5):
-            X = array([x, y, z])
-            q = dot(K, X)
-            q = [int(q[0]/q[2]), int(q[1]/q[2])]
-            im[h-q[1], q[0]] = 255
 
 dim = 3
 if __name__ == "__main__":    
     fin = sys.argv[1]
     cap = cv2.VideoCapture(fin)
-    kalman = Kalman(K, mu_init=array([1., 1., 165., 2.]))
-
-    while (True):
+    N = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+    fps = int(cap.get(cv2.CAP_PROP_FPS))
+    kalman = Kalman(util.K, mu_init=array([1., 1., 165., 0.5]))
+    
+    for i in range(N):
         ret, frame = cap.read()
         h,w = frame.shape[:2]
-        #proj_board(frame, 1, 1, 160)
+        #proj_board(frame, 1, 1, 160) # basla
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
         status, corners = cv2.findChessboardCorners( gray, (dim,dim))
         is_x = []; is_y = []
@@ -33,14 +26,12 @@ if __name__ == "__main__":
 
         if len(is_x) > 0 : 
             kalman.update(array([is_x[5], h-is_y[5], 1.]))
-            print 'projecting'
-            proj_board(gray, 
-                       kalman.mu_hat[0], 
-                       kalman.mu_hat[1], 
-                       kalman.mu_hat[2])
-                
+            util.proj_board(gray, 
+                            kalman.mu_hat[0], 
+                            kalman.mu_hat[1], 
+                            kalman.mu_hat[2])
+
         cv2.imshow('frame',gray)
-        if cv2.waitKey(20) & 0xFF == ord('q'):
-            break        
+        cv2.waitKey(20)
     
        
--- vision/vision_60track/track-chess-pf.py
+++ vision/vision_60track/track-chess-pf.py
@@ -1,24 +1,18 @@
-import cv2
 import sys; sys.path.append('../../tser/tser_pf')
+import cv2
+import util
 from PF import *
-from K import *
-
-def proj_board(im, xl, yl, z):
-    h,w = im.shape[:2]
-    for x in arange(xl-9, xl+9, 0.5):
-        for y in arange(yl-9, yl+9, 0.5):
-            X = array([x, y, z])
-            q = dot(K, X)
-            q = [int(q[0]/q[2]), int(q[1]/q[2])]
-            im[h-q[1], q[0]] = 255
 
 dim = 3
 if __name__ == "__main__":    
+
     fin = sys.argv[1]
     cap = cv2.VideoCapture(fin)
-    pf = PF(K, 200)
+    N = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
 
-    while (True):
+    pf = PF(util.K, 200)
+
+    for i in range(N):
         ret, frame = cap.read()
         h,w = frame.shape[:2]
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
@@ -33,8 +27,8 @@ if __name__ == "__main__":
         if len(is_x) > 0: 
             pf.update(array([is_x[5], h-is_y[5], 1.]))
             mu_x = pf.average()
-            proj_board(gray, mu_x[0], mu_x[1], mu_x[2])
-                
+            util.proj_board(gray, mu_x[0], mu_x[1], mu_x[2])
+
         cv2.imshow('frame',gray)
         if cv2.waitKey(20) & 0xFF == ord('q'):
             break        
