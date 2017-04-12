c07fff7bdd4e57dadfecd7b7db3f86c29536269c
BB <bb@b.om>
2017-04-12 13:03:30 +0200

    -

--- vision/vision_02/plot3d.py
+++ vision/vision_02/plot3d.py
@@ -5,16 +5,10 @@ import numpy as np
 def plot_vector(fig, orig, v, color='blue'):
    ax = fig.gca(projection='3d')
    orig = np.array(orig); v=np.array(v)
-   length = np.sqrt((v**2).sum())
-   # quiver kodu ikinci uclu kordinati vektorun 'ucu' olarak aliyor, 
-   # biz de burada baslangic noktasi ve vektor parametrelerini ona 
-   # gore degistiriyoruz.
-   tip = orig + v
-   ax.quiver(tip[0], tip[1], tip[2], v[0], v[1], v[2],length=length,color=color)
+   ax.quiver(orig[0], orig[1], orig[2], v[0], v[1], v[2],color=color)
    ax.set_xlim(0,10);ax.set_ylim(0,10);ax.set_zlim(0,10)
    ax = fig.gca(projection='3d')  
    return fig
-
 
 def plot_plane(fig, point, normal, color='yellow'):
     point = np.array(point)
