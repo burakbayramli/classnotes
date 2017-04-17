526319316bb5a376f538cd4882afd1bae84e7084
BB <bb@b.om>
2017-04-17 10:52:36 +0200

    -

--- vision/vision_02/plot3d.py
+++ vision/vision_02/plot3d.py
@@ -1,5 +1,8 @@
 from mpl_toolkits.mplot3d import axes3d
+from matplotlib.patches import Circle, PathPatch
 import matplotlib.pyplot as plt
+from matplotlib.transforms import Affine2D
+from mpl_toolkits.mplot3d import art3d
 import numpy as np
 
 def plot_vector(fig, orig, v, color='blue'):
@@ -10,15 +13,47 @@ def plot_vector(fig, orig, v, color='blue'):
    ax = fig.gca(projection='3d')  
    return fig
 
-def plot_plane(fig, point, normal, color='yellow'):
-    point = np.array(point)
-    normal = np.array(normal)
-    # duzlem a*x+b*y+c*z+d=0 olarak temsil edilir, duzlemin normali
-    # [a,b,c]. O yuzden d'yi hesaplamak lazim.
-    d = -point.dot(normal)
-    xx, yy = np.meshgrid(range(10), range(10))
-    # x,y lere tekabul eden z'leri hesapla
-    z = (-normal[0] * xx - normal[1] * yy - d) * 1. /normal[2]
-    # yuzeyi grafikle
-    plt3d = fig.gca(projection='3d')
-    plt3d.plot_surface(xx, yy, z, alpha=0.3,color='yellow')
+def rotation_matrix(d):
+    sin_angle = np.linalg.norm(d)
+    if sin_angle == 0:return np.identity(3)
+    d /= sin_angle
+    eye = np.eye(3)
+    ddt = np.outer(d, d)
+    skew = np.array([[    0,  d[2],  -d[1]],
+                  [-d[2],     0,  d[0]],
+                  [d[1], -d[0],    0]], dtype=np.float64)
+
+    M = ddt + np.sqrt(1 - sin_angle**2) * (eye - ddt) + sin_angle * skew
+    return M
+
+def pathpatch_2d_to_3d(pathpatch, z, normal):
+    if type(normal) is str: #Translate strings to normal vectors
+        index = "xyz".index(normal)
+        normal = np.roll((1.0,0,0), index)
+
+    normal /= np.linalg.norm(normal) #Make sure the vector is normalised
+    path = pathpatch.get_path() #Get the path and the associated transform
+    trans = pathpatch.get_patch_transform()
+
+    path = trans.transform_path(path) #Apply the transform
+
+    pathpatch.__class__ = art3d.PathPatch3D #Change the class
+    pathpatch._code3d = path.codes #Copy the codes
+    pathpatch._facecolor3d = pathpatch.get_facecolor #Get the face color    
+
+    verts = path.vertices #Get the vertices in 2D
+
+    d = np.cross(normal, (0, 0, 1)) #Obtain the rotation vector    
+    M = rotation_matrix(d) #Get the rotation matrix
+
+    pathpatch._segment3d = np.array([np.dot(M, (x, y, 0)) + (0, 0, z) for x, y in verts])
+
+def pathpatch_translate(pathpatch, delta):
+    pathpatch._segment3d += delta
+
+def plot_plane(ax, point, normal, size=10, color='y'):    
+    p = Circle((0, 0), size, facecolor = color, alpha = .2)
+    ax.add_patch(p)
+    pathpatch_2d_to_3d(p, z=0, normal=normal)
+    pathpatch_translate(p, (point[0], point[1], point[2]))
+    
