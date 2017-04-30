d097515bb888266680b8b26039299478cacf6c4f
BB <bb@b.om>
2017-04-30 22:09:50 +0200

    -

--- vision/vision_97gps1/orbital.py
+++ vision/vision_97gps1/orbital.py
@@ -1,4 +1,5 @@
-import datetime
+from datetime import datetime
+#import datetime
 import numpy as np
 
 F = 1 / 298.257223563 # Dunya duzlestirme WGS-84
@@ -8,19 +9,12 @@ F = 1 / 298.257223563  # Dunya duzlestirme WGS-84
 A = 6378.137  # WGS84 ekvotarsal cap
 
 def jdays2000(utc_time):
-    """2000'den itibaren gecen gunler
-    """
     return _days(utc_time - datetime(2000, 1, 1, 12, 0))
     
-
 def jdays(utc_time):
-    """UTC zamaninini julyen zamanina degistir
-    """
     return jdays2000(utc_time) + 2451545
 
 def _fdays(dt):
-    """d_t'den reel sayi bazinda gunleri al
-    """
     return (dt.days +
             (dt.seconds +
              dt.microseconds / (1000000.0)) / (24 * 3600.0))
@@ -28,55 +22,33 @@ def _fdays(dt):
 _vdays = np.vectorize(_fdays)
 
 def _days(dt):
-    """Get the days (floating point) from *d_t*.
-    """
     try:
         return _fdays(dt)
     except AttributeError:
         return _vdays(dt)
 
 def gmst(utc_time):
-    """Greenwich mean sidereal utc_time, in radians.    
-    As defined in the AIAA 2006 implementation:
-    http://www.celestrak.com/publications/AIAA/2006-6753/
-    """
     ut1 = jdays2000(utc_time) / 36525.0
     theta = 67310.54841 + ut1 * (876600 * 3600 + 8640184.812866 + ut1 *
                                  (0.093104 - ut1 * 6.2 * 10e-6))
-    return np.deg2rad(theta / 240.0) % (2 * np.pi)
-    
+    return np.deg2rad(theta / 240.0) % (2 * np.pi)    
 
 def observer_position(time, lon, lat, alt):
-    """Calculate observer ECI position.
-    http://celestrak.com/columns/v02n03/
-    """
-    
     lon = np.deg2rad(lon)
-    lat = np.deg2rad(lat)
-    
+    lat = np.deg2rad(lat)    
     theta = (gmst(time) + lon) % (2 * np.pi)
     c = 1 / np.sqrt(1 + F * (F - 2) * np.sin(lat)**2)
     sq = c * (1 - F)**2
-
     achcp = (A * c + alt) * np.cos(lat)
     x = achcp * np.cos(theta)  # kilometers
     y = achcp * np.sin(theta)
     z = (A * sq + alt) * np.sin(lat)
-
     vx = -MFACTOR*y  # kilometers/second
     vy = MFACTOR*x
     vz = 0
     return (x, y, z), (vx, vy, vz)   
 
 def get_observer_look(sat_lon, sat_lat, sat_alt, utc_time, lon, lat, alt):
-    """Calculate observers look angle to a satellite.
-    http://celestrak.com/columns/v02n02/
-    utc_time: Observation time (datetime object)
-    lon: Longitude of observer position on ground
-    lat: Latitude of observer position on ground
-    alt: Altitude above sea-level (geoid) of observer position on ground
-    Return: (Azimuth, Elevation)
-    """
     (pos_x, pos_y, pos_z), (vel_x, vel_y, vel_z) = \
         observer_position(utc_time, sat_lon, sat_lat, sat_alt)
 
