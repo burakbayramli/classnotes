6db994df9ef9ac18186ae0ffe9aef7f3aae79d90
BB <bb@b.om>
2017-06-15 21:24:19 +0200

    -

---
+++ linear/linear_app05pca/matlab/CCIPCA2.m
@@ -0,0 +1,21 @@
+function [eigval, eigvect] = CCIPCA2(lambda, U, x, n, q)
+
+tol=1e-7;
+
+k =length(lambda);
+q =int32(q);
+x=x';
+V=zeros(size(U));
+for jjj=1:q  
+    u_=U(:,jjj);
+    v_=n/(n+1)*lambda(jjj)*u_+1/(n+1)*(x'*u_)*x;    
+    nrm=norm(v_);    
+    u_=v_/nrm;
+    lambda(jjj)=nrm;
+    x=x-u_*(u_'*x);
+    U(:,jjj)=u_;
+end
+
+[~,idx]=sort(lambda,'descend');
+eigval=lambda(idx);
+eigvect=U(:,idx);
---
+++ linear/linear_app05pca/matlab/tst.m
@@ -0,0 +1,37 @@
+load irisdata.txt
+
+D = 4;
+q = 2;
+X = irisdata(:,1:D);
+n = size(X,2);
+Xmean = mean(X,2);           % find mean
+A = X - Xmean*ones(1,n);    % subtract mean from each point
+rho = norm(A,'fro')^2       % total variation of data
+[U,S,V] = svd(A,'econ');    % find singular value decomposition
+[U1,S1,V1] = svd(A(1:10,:),'econ');    % find singular value decomposition
+sigma = diag(S);
+sigma
+V
+
+%[V,D,W] = svd(cov(A(1:10,:)));
+%values=diag(D);
+%eigvect=V;
+%values=values(1:q);
+%vectors=eigvect(:,1:q);
+
+values=normrnd(0, 0.2, q, 1);
+vectors=normrnd(0, 0.2, D, q);
+%values=diag(S1)
+%vectors=V1
+
+%values
+%vectors
+
+for i = 1:150
+  [values, vectors] = CCIPCA2(values, vectors, A(i,:),i,2);
+end
+
+
+values
+vectors
+
