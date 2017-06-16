1b53fc68a41ba934fb433ca934eec3374335cc0a
BB <bb@b.om>
2017-06-16 11:27:21 +0200

    -

--- linear/linear_app05pca/matlab/ccipca3.py
+++ linear/linear_app05pca/matlab/ccipca3.py
@@ -1,7 +1,3 @@
-
-# Author: Kevin Hughes <kevinhughes27@gmail.com>
-# License: BSD Style.
-
 import scipy.sparse as sp
 import numpy as np
 from scipy import linalg as la
@@ -9,13 +5,12 @@ import scipy.sparse as sps
 from sklearn import datasets
 
 class CCIPCA:    
-    def __init__(self, n_components=2, amnesic=2.0, copy=True):
-        self.n_components = n_components            
+    def __init__(self, n_components, n_features, amnesic=2.0, copy=True):
+        self.n_components = n_components
+        self.n_features = n_features
         self.copy = copy
         self.amnesic = amnesic
         self.iteration = 0
-        self.n_samples=0
-        self.n_features = 0
         self.mean_ = None
         self.components_ = None
 
@@ -41,17 +36,11 @@ class CCIPCA:
         # update components
         for j in range(0,self.n_components):
             
-            if j > n:
-                # the component has already been init to a zerovec
-                pass
-            
-            elif j == n:
-                # set the component to u 
-                V[j,:] = u
+            if j > n: pass            
+            elif j == n: V[j,:] = u
             else:       
                 # update the components
-                if la.norm(V[j,:]) > 0: 
-                    V[j,:] = w1*V[j,:] + w2*np.dot(u,V[j,:])*u / la.norm(V[j,:])
+                V[j,:] = w1*V[j,:] + w2*np.dot(u,V[j,:])*u / la.norm(V[j,:])
                 normedV = V[j,:] / la.norm(V[j,:])
                 normedV = normedV.reshape((self.n_features, 1))
                 tmp1 = np.dot(u,normedV)
@@ -61,27 +50,47 @@ class CCIPCA:
                 
 
         self.iteration += 1
-        self.components_ = V
+        self.components_ = V / la.norm(V)
             
         return
 
+    def post_process(self):
+        
+        self.explained_variance_ratio_ = np.sqrt(np.sum(self.components_**2,axis=1))
+
+        # sort by explained_variance_ratio_
+        idx = np.argsort(-self.explained_variance_ratio_)
+        self.explained_variance_ratio_ = self.explained_variance_ratio_[idx]
+        self.components_ = self.components_[idx,:]
+
+        # re-normalize
+        self.explained_variance_ratio_ = (self.explained_variance_ratio_ / self.explained_variance_ratio_.sum())
+
+        for r in range(0,self.components_.shape[0]):
+            self.components_[r,:] /= np.sqrt(np.dot(self.components_[r,:],self.components_[r,:]))
+
+    
     
 # Load data
 iris = datasets.load_iris()
 X = iris.data
 y = iris.target
-target_names = iris.target_names
 
-k = 2
-ccipca = CCIPCA(n_components=k)
-ccipca.n_samples=X.shape[0]
-ccipca.n_features = X.shape[1]
+ccipca = CCIPCA(n_components=2,n_features=X.shape[1])
 ccipca.mean_ = np.zeros([ccipca.n_features], np.float)
-ccipca.components_ = np.zeros([ccipca.n_components,ccipca.n_features], np.float)
+#ccipca.components_ = np.zeros([ccipca.n_components,ccipca.n_features], np.float)
+ccipca.components_ = np.ones((ccipca.n_components,ccipca.n_features)) / (ccipca.n_features*ccipca.n_components)
                     
-X = sps.lil_matrix(X)
-
+#X = sps.lil_matrix(X)
+#for i in range(X.shape[0]): ccipca.partial_fit(X[i,:])
 for i in range(X.shape[0]): ccipca.partial_fit(X[i,:])
+ccipca.post_process()
 
 print ccipca.components_
 
+'''
+[[ 0.36158968 -0.08226889  0.85657211  0.35884393]
+ [ 0.65653988  0.72971237 -0.1757674  -0.07470647]]
+[[ 0.81484887  0.52092446  0.25321053  0.02331202]
+ [ 0.09304124 -0.38124227  0.86734622  0.30611795]]
+'''
