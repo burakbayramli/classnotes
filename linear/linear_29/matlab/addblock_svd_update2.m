2dcf08ff55f85695e11d48526b57320e5eb73ba2
BB <bb@b.om>
2017-06-19 20:32:08 +0200

    -

---
+++ linear/linear_29/matlab/addblock_svd_update2.m
@@ -0,0 +1,34 @@
+function [Up,Sp,Vp] = addblock_svd_update2( Uarg, Sarg, Varg, Aarg, force_orth )
+
+  U = Varg;
+  V = Uarg;
+  S = Sarg;
+  A = Aarg';
+  
+  current_rank = size( U, 2 );
+  m = U' * A;
+  p = A - U*m;
+  P = orth( p );
+  P = [ P zeros(size(P,1), size(p,2)-size(P,2)) ];
+  Ra = P' * p;
+  z = zeros( size(m) );
+  K = [ S m ; z' Ra ];
+  [tUp,tSp,tVp] = svds( K, current_rank );
+  Sp = tSp;
+  Up = [ U P ] * tUp;                           
+  if ( ~isempty(V) )
+    Vp = V * tVp( 1:current_rank, : );
+  else
+    Vp = [];
+  end;
+  Vp = [ Vp ; tVp( current_rank+1:size(tVp,1), : ) ];
+  if ( force_orth )
+    [UQ,UR] = qr( Up, 0 );
+    [VQ,VR] = qr( Vp, 0 );
+    [tUp,tSp,tVp] = svds( UR * Sp * VR', current_rank );
+    Up = UQ * tUp;
+    Vp = VQ * tVp;
+    Sp = tSp;
+  end;
+  
+return;
