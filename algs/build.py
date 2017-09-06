0f3bd816b04e762ca7e1c3b5a492a192d86469ac
Burak Bayramli <burak@localhost.com>
2017-09-06 12:49:01 +0200

    -

--- algs/build.py
+++ algs/build.py
@@ -18,6 +18,7 @@ if len(sys.argv) == 1:
     ./convnet/convnet.pdf \
     ./autodiff/autodiff.pdf \
     ./rnn/rnn.pdf \
+    ./lstm/lstm.pdf \
     ./enc/enc.pdf \
     ./kmeans/kmeans.pdf \
     ./kmeans_mr/kmeans_mr.pdf \
