63ec21042556cfea597f8e94d9bb6154925439cb
Burak Bayramli <burak@localhost.com>
2017-09-07 15:43:54 +0200

    -

--- algs/tensorflow/birth.py
+++ algs/tensorflow/birth.py
@@ -1,57 +1,16 @@
-# Using a Multiple Layer Network
-#---------------------------------------
-#
-# We will illustrate how to use a Multiple
-# Layer Network in Tensorflow
-#
-# Low Birthrate data:
-#
-#Columns    Variable                                              Abbreviation
-#-----------------------------------------------------------------------------
-# Identification Code                                     ID
-# Low Birth Weight (0 = Birth Weight >= 2500g,            LOW
-#                          1 = Birth Weight < 2500g)
-# Age of the Mother in Years                              AGE
-# Weight in Pounds at the Last Menstrual Period           LWT
-# Race (1 = White, 2 = Black, 3 = Other)                  RACE
-# Smoking Status During Pregnancy (1 = Yes, 0 = No)       SMOKE
-# History of Premature Labor (0 = None  1 = One, etc.)    PTL
-# History of Hypertension (1 = Yes, 0 = No)               HT
-# Presence of Uterine Irritability (1 = Yes, 0 = No)      UI
-# Number of Physician Visits During the First Trimester   FTV
-#                (0 = None, 1 = One, 2 = Two, etc.)
-# Birth Weight in Grams                                   BWT
-#------------------------------
 # The multiple neural network layer we will create will be composed of
 # three fully connected hidden layers, with node sizes 25, 10, and 3
-
-import tensorflow as tf
-import matplotlib.pyplot as plt
-import requests
 import numpy as np
-
-
-#birthdata_url = 'http://faculty.washington.edu/heagerty/Courses/b513/WEB2002/datasets/lowbwt.dat'
-#birth_file = requests.get(birthdata_url)
-#birth_file_text = open("lowbwt.dat").read()
-#birth_data = birth_file_text.split('\r\n')[5:]
-#birth_header = [x for x in birth_data[0].split(' ') if len(x)>=1]
-#birth_data = [[float(x) for x in y.split(' ') if len(x)>=1] for y in birth_data[1:] if len(y)>=1]
-
-# Extract y-target (birth weight)
-#y_vals = np.array([x[10] for x in birth_data])
-# Filter for features of interest
-#cols_of_interest = ['AGE', 'LWT', 'RACE', 'SMOKE', 'PTL', 'HT', 'UI', 'FTV']
-#x_vals = np.array([[x[ix] for ix, feature in enumerate(birth_header) if feature in cols_of_interest] for x in birth_data])
-
+from tensorflow.python.framework import ops
+import matplotlib.pyplot as plt
 import pandas as pd
+import tensorflow as tf
 
 cols_of_interest = ['AGE', 'LWT', 'RACE', 'SMOKE', 'PTL', 'HT', 'UI', 'FTV']
 df = pd.read_csv('lowbwt.dat',sep='\s*',engine='python')
-x_vals = df[cols_of_interest]
-y_vals = df['BWT']
+x_vals = np.array(df[cols_of_interest])
+y_vals = np.array(df['BWT'])
 
-from tensorflow.python.framework import ops
 ops.reset_default_graph()
 
 # Set Seed
@@ -59,17 +18,17 @@ seed = 3
 tf.set_random_seed(seed)
 np.random.seed(seed)
 
-batch_size = 100
+batch_size = 10
 
 # Create graph session 
 sess = tf.Session()
 
 # Split data into train/test = 80%/20%
-train_indices = np.random.choice(len(x_vals), np.round(len(x_vals)*0.8), replace=False)
+tmp = np.random.choice(range(len(x_vals)), size=len(x_vals), replace=False)
+first = int(len(x_vals)*0.80)
+train_indices = tmp[:first]
+test_indices = tmp[first:]
 
-exit()
-
-test_indices = np.array(list(set(range(len(x_vals))) - set(train_indices)))
 x_vals_train = x_vals[train_indices]
 x_vals_test = x_vals[test_indices]
 y_vals_train = y_vals[train_indices]
@@ -156,33 +115,15 @@ for i in range(200):
     test_temp_loss = sess.run(loss, feed_dict={x_data: x_vals_test, y_target: np.transpose([y_vals_test])})
     test_loss.append(test_temp_loss)
     if (i+1)%25==0:
-        print('Generation: ' + str(i+1) + '. Loss = ' + str(temp_loss))
+        print('Epoch: ' + str(i+1) + '. Kayip = ' + str(temp_loss))
 
 
 # Plot loss over time
-plt.plot(loss_vec, 'k-', label='Train Loss')
-plt.plot(test_loss, 'r--', label='Test Loss')
-plt.title('Loss per Generation')
-plt.xlabel('Generation')
-plt.ylabel('Loss')
+plt.plot(loss_vec, 'k-', label='Egitim Kaybi')
+plt.plot(test_loss, 'r--', label='Test Kaybi')
+plt.title('Her Epoch Icinde Kayip')
+plt.xlabel('Epoch')
+plt.ylabel('Kayip')
 plt.legend(loc="upper right")
-plt.show()
+plt.savefig('tf_03.png')
 
-# Find the % classified correctly above/below the cutoff of 2500 g
-# >= 2500 g = 0
-# < 2500 g = 1
-actuals = np.array([x[1] for x in birth_data])
-test_actuals = actuals[test_indices]
-train_actuals = actuals[train_indices]
-
-test_preds = [x[0] for x in sess.run(final_output, feed_dict={x_data: x_vals_test})]
-train_preds = [x[0] for x in sess.run(final_output, feed_dict={x_data: x_vals_train})]
-test_preds = np.array([1.0 if x<2500.0 else 0.0 for x in test_preds])
-train_preds = np.array([1.0 if x<2500.0 else 0.0 for x in train_preds])
-
-# Print out accuracies
-test_acc = np.mean([x==y for x,y in zip(test_preds, test_actuals)])
-train_acc = np.mean([x==y for x,y in zip(train_preds, train_actuals)])
-print('On predicting the category of low birthweight from regression output (<2500g):')
-print('Test Accuracy: {}'.format(test_acc))
-print('Train Accuracy: {}'.format(train_acc))
