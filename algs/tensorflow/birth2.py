304d81b844b7e0db19876de4b2711dfa46678477
Burak Bayramli <burak@localhost.com>
2017-09-08 13:20:10 +0200

    -

--- algs/tensorflow/birth2.py
+++ algs/tensorflow/birth2.py
@@ -9,16 +9,12 @@ import tensorflow as tf
 cols_of_interest = ['AGE', 'LWT', 'RACE', 'SMOKE', 'PTL', 'HT', 'UI', 'FTV']
 df = pd.read_csv('lowbwt.dat',sep='\s*',engine='python')
 x_vals = np.array(df[cols_of_interest])
-y_vals = np.array(df['BWT'])
-
-init = tf.initialize_all_variables ()
+y_vals = (np.array(df['BWT']) < 2500.0).astype(float)
 
 # Set Seed
 seed = 3
 tf.set_random_seed(seed)
 np.random.seed(seed)
-
-batch_size = 10
 
 # Create graph session 
 sess = tf.Session()
@@ -48,7 +44,7 @@ x_vals_test = np.nan_to_num(normalize_cols(x_vals_test))
 batch_size = 90
 
 # Initialize placeholders
-x_data = tf.placeholder(shape=[None, 7], dtype=tf.float32)
+x_data = tf.placeholder(shape=[None, 8], dtype=tf.float32)
 y_target = tf.placeholder(shape=[None, 1], dtype=tf.float32)
 
 
@@ -69,7 +65,7 @@ def logistic(input_layer, multiplication_weight, bias_weight, activation = True)
 
 
 # First logistic layer (7 inputs to 7 hidden nodes)
-A1 = init_variable(shape=[7,14])
+A1 = init_variable(shape=[8,14])
 b1 = init_variable(shape=[14])
 logistic_layer1 = logistic(x_data, A1, b1)
 
@@ -91,7 +87,7 @@ my_opt = tf.train.AdamOptimizer(learning_rate = 0.005)
 train_step = my_opt.minimize(loss)
 
 # Initialize variables
-init = tf.initialize_all_variables()
+init = tf.global_variables_initializer()
 sess.run(init)
 
 # Actual Prediction
@@ -99,3 +95,37 @@ prediction = tf.round(tf.nn.sigmoid(final_output))
 predictions_correct = tf.cast(tf.equal(prediction, y_target), tf.float32)
 accuracy = tf.reduce_mean(predictions_correct)
 
+# Training loop
+loss_vec = []
+train_acc = []
+test_acc = []
+for i in range(1500):
+    rand_index = np.random.choice(len(x_vals_train), size=batch_size)
+    rand_x = x_vals_train[rand_index]
+    rand_y = np.transpose([y_vals_train[rand_index]])
+    sess.run(train_step, feed_dict={x_data: rand_x, y_target: rand_y})
+    temp_loss = sess.run(loss, feed_dict={x_data: rand_x, y_target: rand_y})
+    loss_vec.append(temp_loss)
+    temp_acc_train = sess.run(accuracy, feed_dict={x_data: x_vals_train, y_target: np.transpose([y_vals_train])})
+    train_acc.append(temp_acc_train)
+    temp_acc_test = sess.run(accuracy, feed_dict={x_data: x_vals_test, y_target: np.transpose([y_vals_test])})
+    test_acc.append(temp_acc_test)
+    if (i+1)%150==0:
+        print('Loss = ' + str(temp_loss))
+
+# Plot loss over time
+plt.plot(loss_vec, 'k-')
+plt.title('Cross Entropy Loss per Generation')
+plt.xlabel('Generation')
+plt.ylabel('Cross Entropy Loss')
+plt.show()
+
+# Plot train and test accuracy
+plt.plot(train_acc, 'k-', label='Train Set Accuracy')
+plt.plot(test_acc, 'r--', label='Test Set Accuracy')
+plt.title('Train and Test Accuracy')
+plt.xlabel('Generation')
+plt.ylabel('Accuracy')
+plt.legend(loc='lower right')
+plt.show()
+        
