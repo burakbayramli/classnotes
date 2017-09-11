66d841f8915bbc0b3236746286467db702a37d74
Burak Bayramli <burak@localhost.com>
2017-09-11 13:42:18 +0200

    -

--- algs/tensorflow/birth1.py
+++ algs/tensorflow/birth1.py
@@ -1,8 +1,7 @@
-# The multiple neural network layer we will create will be composed of
-# three fully connected hidden layers, with node sizes 25, 10, and 3
-import numpy as np
-from tensorflow.python.framework import ops
 import matplotlib.pyplot as plt
+import numpy as np
+
+from tensorflow.python.framework import ops
 import pandas as pd
 import tensorflow as tf
 
@@ -12,18 +11,13 @@ x_vals = np.array(df[cols_of_interest])
 y_vals = np.array(df['BWT'])
 
 ops.reset_default_graph()
-
-# Set Seed
-seed = 3
-tf.set_random_seed(seed)
-np.random.seed(seed)
+tf.set_random_seed(3)
+np.random.seed(3)
 
 batch_size = 10
 
-# Create graph session 
 sess = tf.Session()
 
-# Split data into train/test = 80%/20%
 tmp = np.random.choice(range(len(x_vals)), size=len(x_vals), replace=False)
 first = int(len(x_vals)*0.80)
 train_indices = tmp[:first]
@@ -35,7 +29,6 @@ y_vals_train = y_vals[train_indices]
 y_vals_test = y_vals[test_indices]
 
 
-# Normalize by column (min-max norm to be between 0 and 1)
 def normalize_cols(m):
     col_max = m.max(axis=0)
     col_min = m.min(axis=0)
@@ -44,65 +37,48 @@ def normalize_cols(m):
 x_vals_train = np.nan_to_num(normalize_cols(x_vals_train))
 x_vals_test = np.nan_to_num(normalize_cols(x_vals_test))
 
-
-# Define Variable Functions (weights and bias)
 def init_weight(shape, st_dev):
     weight = tf.Variable(tf.random_normal(shape, stddev=st_dev))
     return(weight)
     
-
 def init_bias(shape, st_dev):
     bias = tf.Variable(tf.random_normal(shape, stddev=st_dev))
     return(bias)
     
-    
-# Create Placeholders
 x_data = tf.placeholder(shape=[None, 8], dtype=tf.float32)
 y_target = tf.placeholder(shape=[None, 1], dtype=tf.float32)
 
-
-# Create a fully connected layer:
 def fully_connected(input_layer, weights, biases):
     layer = tf.add(tf.matmul(input_layer, weights), biases)
     return(tf.nn.relu(layer))
 
-
-#--------Create the first layer (25 hidden nodes)--------
 weight_1 = init_weight(shape=[8, 25], st_dev=10.0)
 bias_1 = init_bias(shape=[25], st_dev=10.0)
 layer_1 = fully_connected(x_data, weight_1, bias_1)
 
-#--------Create second layer (10 hidden nodes)--------
 weight_2 = init_weight(shape=[25, 10], st_dev=10.0)
 bias_2 = init_bias(shape=[10], st_dev=10.0)
 layer_2 = fully_connected(layer_1, weight_2, bias_2)
 
-
-#--------Create third layer (3 hidden nodes)--------
 weight_3 = init_weight(shape=[10, 3], st_dev=10.0)
 bias_3 = init_bias(shape=[3], st_dev=10.0)
 layer_3 = fully_connected(layer_2, weight_3, bias_3)
 
-
-#--------Create output layer (1 output value)--------
 weight_4 = init_weight(shape=[3, 1], st_dev=10.0)
 bias_4 = init_bias(shape=[1], st_dev=10.0)
 final_output = fully_connected(layer_3, weight_4, bias_4)
 
-# Declare loss function (L1)
 loss = tf.reduce_mean(tf.abs(y_target - final_output))
 
-# Declare optimizer
 my_opt = tf.train.AdamOptimizer(0.05)
 train_step = my_opt.minimize(loss)
 
-# Initialize Variables
 init = tf.initialize_all_variables()
 sess.run(init)
 
 # Training loop
-loss_vec = []
-test_loss = []
+loss_vec = []; test_loss = []
+
 for i in range(200):
     rand_index = np.random.choice(len(x_vals_train), size=batch_size)
     rand_x = x_vals_train[rand_index]
--- algs/tensorflow/birth2.py
+++ algs/tensorflow/birth2.py
@@ -1,25 +1,22 @@
-# The multiple neural network layer we will create will be composed of
-# three fully connected hidden layers, with node sizes 25, 10, and 3
+# -*- coding: utf-8 -*-
 import numpy as np
-from tensorflow.python.framework import ops
 import matplotlib.pyplot as plt
+
+from tensorflow.python.framework import ops
 import pandas as pd
 import tensorflow as tf
 
 cols_of_interest = ['AGE', 'LWT', 'RACE', 'SMOKE', 'PTL', 'HT', 'UI', 'FTV']
 df = pd.read_csv('lowbwt.dat',sep='\s*',engine='python')
 x_vals = np.array(df[cols_of_interest])
-y_vals = (np.array(df['BWT']) < 2500.0).astype(float)
+y_vals = np.array(df['LOW'])
 
-# Set Seed
-seed = 3
+seed = 1
 tf.set_random_seed(seed)
 np.random.seed(seed)
 
-# Create graph session 
 sess = tf.Session()
 
-# Split data into train/test = 80%/20%
 tmp = np.random.choice(range(len(x_vals)), size=len(x_vals), replace=False)
 first = int(len(x_vals)*0.80)
 train_indices = tmp[:first]
@@ -30,8 +27,6 @@ x_vals_test = x_vals[test_indices]
 y_vals_train = y_vals[train_indices]
 y_vals_test = y_vals[test_indices]
 
-
-# Normalize by column (min-max norm to be between 0 and 1)
 def normalize_cols(m):
     col_max = m.max(axis=0)
     col_min = m.min(axis=0)
@@ -40,65 +35,47 @@ def normalize_cols(m):
 x_vals_train = np.nan_to_num(normalize_cols(x_vals_train))
 x_vals_test = np.nan_to_num(normalize_cols(x_vals_test))
 
-# Declare batch size
-batch_size = 90
+batch_size = 130
 
-# Initialize placeholders
 x_data = tf.placeholder(shape=[None, 8], dtype=tf.float32)
 y_target = tf.placeholder(shape=[None, 1], dtype=tf.float32)
 
-
-# Create variable definition
 def init_variable(shape):
     return(tf.Variable(tf.random_normal(shape=shape)))
 
-
-# Create a logistic layer definition
 def logistic(input_layer, multiplication_weight, bias_weight, activation = True):
     linear_layer = tf.add(tf.matmul(input_layer, multiplication_weight), bias_weight)
-    # We separate the activation at the end because the loss function will
-    # implement the last sigmoid necessary
     if activation:
         return(tf.nn.sigmoid(linear_layer))
     else:
         return(linear_layer)
 
-
-# First logistic layer (7 inputs to 7 hidden nodes)
-A1 = init_variable(shape=[8,14])
-b1 = init_variable(shape=[14])
+A1 = init_variable(shape=[8,20])
+b1 = init_variable(shape=[20])
 logistic_layer1 = logistic(x_data, A1, b1)
 
-# Second logistic layer (7 hidden inputs to 5 hidden nodes)
-A2 = init_variable(shape=[14,5])
-b2 = init_variable(shape=[5])
+A2 = init_variable(shape=[20,10])
+b2 = init_variable(shape=[10])
 logistic_layer2 = logistic(logistic_layer1, A2, b2)
 
-# Final output layer (5 hidden nodes to 1 output)
-A3 = init_variable(shape=[5,1])
+A3 = init_variable(shape=[10,1])
 b3 = init_variable(shape=[1])
 final_output = logistic(logistic_layer2, A3, b3, activation=False)
 
-# Declare loss function (Cross Entropy loss)
 loss = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(logits=final_output, labels=y_target))
      
-# Declare optimizer
-my_opt = tf.train.AdamOptimizer(learning_rate = 0.005)
+my_opt = tf.train.AdamOptimizer(learning_rate = 0.002)
 train_step = my_opt.minimize(loss)
 
-# Initialize variables
 init = tf.global_variables_initializer()
 sess.run(init)
 
-# Actual Prediction
 prediction = tf.round(tf.nn.sigmoid(final_output))
 predictions_correct = tf.cast(tf.equal(prediction, y_target), tf.float32)
 accuracy = tf.reduce_mean(predictions_correct)
 
-# Training loop
-loss_vec = []
-train_acc = []
-test_acc = []
+loss_vec = []; train_acc = []; test_acc = []
+
 for i in range(1500):
     rand_index = np.random.choice(len(x_vals_train), size=batch_size)
     rand_x = x_vals_train[rand_index]
@@ -113,19 +90,19 @@ for i in range(1500):
     if (i+1)%150==0:
         print('Loss = ' + str(temp_loss))
 
-# Plot loss over time
 plt.plot(loss_vec, 'k-')
-plt.title('Cross Entropy Loss per Generation')
-plt.xlabel('Generation')
-plt.ylabel('Cross Entropy Loss')
-plt.show()
+plt.title(u"Her Epoch İçin Çapraz Entropi Kaybı")
+plt.xlabel(u"Epoch")
+plt.ylabel(u"Çapraz Entropi Kaybı")
+plt.ylim(0.0,2.0)
+plt.savefig('tf_04.png')        
 
-# Plot train and test accuracy
-plt.plot(train_acc, 'k-', label='Train Set Accuracy')
-plt.plot(test_acc, 'r--', label='Test Set Accuracy')
-plt.title('Train and Test Accuracy')
-plt.xlabel('Generation')
-plt.ylabel('Accuracy')
+plt.figure()
+plt.plot(train_acc, 'k-', label=u"Eğitim Seti Doğrulugu")
+plt.plot(test_acc, 'r--', label=u"Test Set Doğrulugu")
+plt.title(u"Eğitim ve Test Doğruluğu")
+plt.xlabel("Epoch")
+plt.ylabel(u"Doğruluk")
+plt.ylim(0.0,0.8)
 plt.legend(loc='lower right')
-plt.show()
-        
+plt.savefig('tf_05.png')        
