6981bdce675def53f6bf4b17a0b638759123a9f5
Burak Bayramli <burak@localhost.com>
2017-09-14 12:20:33 +0200

    -

--- algs/reinf/reinf3.py
+++ algs/reinf/reinf3.py
@@ -13,13 +13,6 @@ def reset_graph(seed=42):
 def render_cart_pole(env, obs):
     return env.render(mode="rgb_array")
 
-def plot_animation(frames, repeat=False, interval=40):
-    plt.close()  # or else nbagg sometimes plots in the previous cell
-    fig = plt.figure()
-    patch = plt.imshow(frames[0])
-    plt.axis('off')
-    return animation.FuncAnimation(fig, update_scene, fargs=(frames, patch), frames=len(frames), repeat=repeat, interval=interval)
-
 def update_scene(num, frames, patch):
     patch.set_data(frames[num])
     return patch,
@@ -40,6 +33,49 @@ def render_policy_net(model_path, action, X, n_max_steps = 1000):
     env.close()
     return frames
 
-frames = render_policy_net("/tmp/my_policy_net_pg.ckpt", action, X, n_max_steps=1000)
-video = plot_animation(frames)
-plt.show()
+import tensorflow as tf
+
+reset_graph()
+
+n_inputs = 4
+n_hidden = 4
+n_outputs = 1
+
+learning_rate = 0.01
+
+initializer = tf.contrib.layers.variance_scaling_initializer()
+
+X = tf.placeholder(tf.float32, shape=[None, n_inputs])
+
+hidden = tf.layers.dense(X, n_hidden, activation=tf.nn.elu, kernel_initializer=initializer)
+logits = tf.layers.dense(hidden, n_outputs)
+outputs = tf.nn.sigmoid(logits)  # probability of action 0 (left)
+p_left_and_right = tf.concat(axis=1, values=[outputs, 1 - outputs])
+action = tf.multinomial(tf.log(p_left_and_right), num_samples=1)
+
+y = 1. - tf.to_float(action)
+cross_entropy = tf.nn.sigmoid_cross_entropy_with_logits(labels=y, logits=logits)
+optimizer = tf.train.AdamOptimizer(learning_rate)
+grads_and_vars = optimizer.compute_gradients(cross_entropy)
+gradients = [grad for grad, variable in grads_and_vars]
+gradient_placeholders = []
+grads_and_vars_feed = []
+for grad, variable in grads_and_vars:
+    gradient_placeholder = tf.placeholder(tf.float32, shape=grad.get_shape())
+    gradient_placeholders.append(gradient_placeholder)
+    grads_and_vars_feed.append((gradient_placeholder, variable))
+training_op = optimizer.apply_gradients(grads_and_vars_feed)
+
+init = tf.global_variables_initializer()
+saver = tf.train.Saver()        
+
+env = gym.make("CartPole-v0")
+
+n_games_per_update = 10
+n_max_steps = 1000
+n_iterations = 250
+save_iterations = 10
+discount_rate = 0.95
+ffile = "/tmp/my_policy_net_pg.ckpt"
+
+frames = render_policy_net("/tmp/my_policy_net_pg.ckpt", action, X, n_max_steps=500)
