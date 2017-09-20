b22e671f81aa3809779e20b8b61168f14471d2cd
Burak Bayramli <burak@localhost.com>
2017-09-20 13:54:33 +0200

    -

--- algs/churn/k1.py
+++ algs/churn/k1.py
@@ -26,15 +26,6 @@ def _keras_unstack_hack(ab):
         b = ab[..., 1]
     return a, b
 
-def weibull_loss_discrete(y_true, y_pred, name=None):
-    y,u = _keras_unstack_hack(y_true)
-    a,b = _keras_unstack_hack(y_pred)
-    hazard0 = K.pow((y + 1e-35) / a, b)
-    hazard1 = K.pow((y + 1.0) / a, b)    
-    loglikelihoods = u * K.log(K.exp(hazard1 - hazard0) - 1.0) - hazard1
-    loss = -1 * K.mean(loglikelihoods)
-    return loss
-
 
 def get_data(n_timesteps, every_nth,n_repeats,noise_level,n_features,use_censored = True):
     def get_equal_spaced(n, every_nth):
@@ -87,6 +78,14 @@ def get_data(n_timesteps, every_nth,n_repeats,noise_level,n_features,use_censore
     x_train = x_train+noise-x_train*noise
     return y_train,x_train, y_test,x_test,events
 
+def weibull_loss_discrete(y_true, y_pred, name=None):
+    y,u = _keras_unstack_hack(y_true)
+    a,b = _keras_unstack_hack(y_pred)
+    hazard0 = K.pow((y + 1e-35) / a, b)
+    hazard1 = K.pow((y + 1.0) / a, b)    
+    loglikelihoods = u * K.log(K.exp(hazard1 - hazard0) - 1.0) - hazard1
+    loss = -1 * K.mean(loglikelihoods)
+    return loss
 
 def output_lambda(x, init_alpha=1.0, max_beta_value=5.0, max_alpha_value=None):
     print x.shape
@@ -108,8 +107,6 @@ def output_lambda(x, init_alpha=1.0, max_beta_value=5.0, max_alpha_value=None):
     b = m * K.clip(x=b, min_value=K.epsilon(), max_value=1. - K.epsilon())
     x = K.stack([a, b], axis=-1)
     return x
-
-
 
 
 n_timesteps    = 200
