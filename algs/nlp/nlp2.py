d9490beb433b7a47879f0c0deb5419e13bcfafc6
Burak Bayramli <burak@localhost.com>
2017-09-25 15:59:09 +0200

    -

--- algs/nlp/nlp2.py
+++ algs/nlp/nlp2.py
@@ -7,31 +7,7 @@ import time
 import datetime
 import data_helpers
 from tensorflow.contrib import learn
-
-# Parameters
-# ==================================================
-
-# Data loading params
-tf.flags.DEFINE_float("dev_sample_percentage", .1, "Percentage of the training data to use for validation")
-tf.flags.DEFINE_string("positive_data_file", "./data/rt-polarity.pos", "Data source for the positive data.")
-tf.flags.DEFINE_string("negative_data_file", "./data/rt-polarity.neg", "Data source for the negative data.")
-
-# Model Hyperparameters
-tf.flags.DEFINE_integer("embedding_dim", 128, "Dimensionality of character embedding (default: 128)")
-tf.flags.DEFINE_string("filter_sizes", "3,4,5", "Comma-separated filter sizes (default: '3,4,5')")
-tf.flags.DEFINE_integer("num_filters", 128, "Number of filters per filter size (default: 128)")
-tf.flags.DEFINE_float("dropout_keep_prob", 0.5, "Dropout keep probability (default: 0.5)")
-tf.flags.DEFINE_float("l2_reg_lambda", 0.0, "L2 regularization lambda (default: 0.0)")
-
-# Training parameters
-tf.flags.DEFINE_integer("batch_size", 64, "Batch Size (default: 64)")
-tf.flags.DEFINE_integer("num_epochs", 200, "Number of training epochs (default: 200)")
-tf.flags.DEFINE_integer("evaluate_every", 100, "Evaluate model on dev set after this many steps (default: 100)")
-tf.flags.DEFINE_integer("checkpoint_every", 100, "Save model after this many steps (default: 100)")
-tf.flags.DEFINE_integer("num_checkpoints", 5, "Number of checkpoints to store (default: 5)")
-# Misc Parameters
-tf.flags.DEFINE_boolean("allow_soft_placement", True, "Allow device soft device placement")
-tf.flags.DEFINE_boolean("log_device_placement", False, "Log placement of ops on devices")
+import constants
 
 FLAGS = tf.flags.FLAGS
 FLAGS._parse_flags()
@@ -41,10 +17,6 @@ for attr, value in sorted(FLAGS.__flags.items()):
 print("")
 
 
-# Data Preparation
-# ==================================================
-
-# Load data
 print("Loading data...")
 x_text, y = data_helpers.load_data_and_labels(FLAGS.positive_data_file, FLAGS.negative_data_file)
 
@@ -67,25 +39,14 @@ y_train, y_dev = y_shuffled[:dev_sample_index], y_shuffled[dev_sample_index:]
 print("Vocabulary Size: {:d}".format(len(vocab_processor.vocabulary_)))
 print("Train/Dev split: {:d}/{:d}".format(len(y_train), len(y_dev)))
 
-
-# Training
-# ==================================================
-
 with tf.Graph().as_default():
     session_conf = tf.ConfigProto(
       allow_soft_placement=FLAGS.allow_soft_placement,
       log_device_placement=FLAGS.log_device_placement)
     sess = tf.Session(config=session_conf)
+    
     with sess.as_default():
-        # cnn = TextCNN(
-        #     sequence_length=x_train.shape[1],
-        #     num_classes=y_train.shape[1],
-        #     vocab_size=len(vocab_processor.vocabulary_),
-        #     embedding_size=FLAGS.embedding_dim,
-        #     filter_sizes=list(map(int, FLAGS.filter_sizes.split(","))),
-        #     num_filters=FLAGS.num_filters,
-        #     l2_reg_lambda=FLAGS.l2_reg_lambda)
-
+        
         num_classes=y_train.shape[1]
         sequence_length=x_train.shape[1]
         num_filters=FLAGS.num_filters
@@ -180,28 +141,6 @@ with tf.Graph().as_default():
                 grad_summaries.append(sparsity_summary)
         grad_summaries_merged = tf.summary.merge(grad_summaries)
             
-        # Output directory for models and summaries
-        timestamp = str(int(time.time()))
-        out_dir = os.path.abspath(os.path.join(os.path.curdir, "runs", timestamp))
-        print("Writing to {}\n".format(out_dir))
-
-        # Summaries for loss and accuracy
-        loss_summary = tf.summary.scalar("loss", loss)
-        acc_summary = tf.summary.scalar("accuracy", accuracy)
-
-        # Train Summaries
-        train_summary_op = tf.summary.merge([loss_summary, acc_summary, grad_summaries_merged])
-        train_summary_dir = os.path.join(out_dir, "summaries", "train")
-        train_summary_writer = tf.summary.FileWriter(train_summary_dir, sess.graph)
-
-        # Dev summaries
-        dev_summary_op = tf.summary.merge([loss_summary, acc_summary])
-        dev_summary_dir = os.path.join(out_dir, "summaries", "dev")
-        dev_summary_writer = tf.summary.FileWriter(dev_summary_dir, sess.graph)
-
-        # Write vocabulary
-        vocab_processor.save(os.path.join(out_dir, "vocab"))
-
         # Initialize all variables
         sess.run(tf.global_variables_initializer())
 
@@ -211,17 +150,14 @@ with tf.Graph().as_default():
         # Training loop. For each batch...
         for batch in batches:
             x_batch, y_batch = zip(*batch)
-            #train_step(x_batch, y_batch)
             feed_dict = {
-              input_x: x_batch,
-              input_y: y_batch,
-              dropout_keep_prob: FLAGS.dropout_keep_prob
+                input_x: x_batch,
+                input_y: y_batch,
+                dropout_keep_prob: FLAGS.dropout_keep_prob
             }
-            _, step, summaries, loss, accuracy = sess.run(
-                [train_op, global_step, train_summary_op, loss, accuracy],
-                feed_dict)
-            time_str = datetime.datetime.now().isoformat()
-            print("{}: step {}, loss {:g}, acc {:g}".format(time_str, step, loss, accuracy))
+            res = sess.run(train_op, feed_dict)
+            res = sess.run(accuracy, feed_dict)
+            print res
 
 
 
