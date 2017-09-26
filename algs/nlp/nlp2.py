2a5a00f5784fb8923bfdda0558126977d479b5fc
Burak Bayramli <burak@localhost.com>
2017-09-26 15:21:45 +0200

    -

--- algs/nlp/nlp2.py
+++ algs/nlp/nlp2.py
@@ -1,10 +1,6 @@
 #! /usr/bin/env python
-
 import tensorflow as tf
 import numpy as np
-import os
-import time
-import datetime
 import data_helpers
 from tensorflow.contrib import learn
 import constants
@@ -38,113 +34,102 @@ y_train, y_dev = y_shuffled[:dev_sample_index], y_shuffled[dev_sample_index:]
 print("Vocabulary Size: {:d}".format(len(vocab_processor.vocabulary_)))
 print("Train/Dev split: {:d}/{:d}".format(len(y_train), len(y_dev)))
 
-with tf.Graph().as_default():
-    session_conf = tf.ConfigProto(
-      allow_soft_placement=FLAGS.allow_soft_placement,
-      log_device_placement=FLAGS.log_device_placement)
-    sess = tf.Session(config=session_conf)
-    
-    with sess.as_default():
-        
-        num_classes=y_train.shape[1]
-        sequence_length=x_train.shape[1]
-        num_filters=FLAGS.num_filters
-        filter_sizes=list(map(int, FLAGS.filter_sizes.split(",")))
-        l2_reg_lambda=0.0
-        
-        input_x = tf.placeholder(tf.int32, [None, sequence_length], name="input_x")
-        input_y = tf.placeholder(tf.float32, [None, num_classes], name="input_y")
-        dropout_keep_prob = tf.placeholder(tf.float32, name="dropout_keep_prob")
+tf.reset_default_graph()
 
-        l2_loss = tf.constant(0.0)
-        with tf.device('/cpu:0'), tf.name_scope("embedding"):
-            W = tf.Variable(tf.random_uniform([len(vocab_processor.vocabulary_), FLAGS.embedding_dim], -1.0, 1.0),
-                            name="W")
-            embedded_chars = tf.nn.embedding_lookup(W, input_x)
-            embedded_chars_expanded = tf.expand_dims(embedded_chars, -1)
+num_classes=y_train.shape[1]
+sequence_length=x_train.shape[1]
+num_filters=FLAGS.num_filters
+filter_sizes=list(map(int, FLAGS.filter_sizes.split(",")))
+l2_reg_lambda=0.0
 
+input_x = tf.placeholder(tf.int32, [None, sequence_length], name="input_x")
+input_y = tf.placeholder(tf.float32, [None, num_classes], name="input_y")
+dropout_keep_prob = tf.placeholder(tf.float32, name="dropout_keep_prob")
 
-        # Create a convolution + maxpool layer for each filter size
-        pooled_outputs = []
-        for i, filter_size in enumerate(filter_sizes):
-            filter_shape = [filter_size, FLAGS.embedding_dim, 1, num_filters]
-            W = tf.Variable(tf.truncated_normal(filter_shape, stddev=0.1))
-            b = tf.Variable(tf.constant(0.1, shape=[num_filters]))
-            conv = tf.nn.conv2d(
-                embedded_chars_expanded,
-                W,
-                strides=[1, 1, 1, 1],
-                padding="VALID")
+l2_loss = tf.constant(0.0)
 
-            h = tf.nn.relu(tf.nn.bias_add(conv, b))
+W = tf.Variable(tf.random_uniform([len(vocab_processor.vocabulary_),
+                                   FLAGS.embedding_dim], -1.0, 1.0),
+                name="W")
+embedded_chars = tf.nn.embedding_lookup(W, input_x)
+embedded_chars_expanded = tf.expand_dims(embedded_chars, -1)
 
-            pooled = tf.nn.max_pool(
-                h,
-                ksize=[1, sequence_length - filter_size + 1, 1, 1],
-                strides=[1, 1, 1, 1],
-                padding='VALID',
-                name="pool")
-            pooled_outputs.append(pooled)
-            
-        num_filters_total = num_filters * len(filter_sizes)
-        h_pool = tf.concat(pooled_outputs, 3)
-        h_pool_flat = tf.reshape(h_pool, [-1, num_filters_total])
+pooled_outputs = []
+for i, filter_size in enumerate(filter_sizes):
+    filter_shape = [filter_size, FLAGS.embedding_dim, 1, num_filters]
+    W = tf.Variable(tf.truncated_normal(filter_shape, stddev=0.1))
+    b = tf.Variable(tf.constant(0.1, shape=[num_filters]))
+    conv = tf.nn.conv2d(
+        embedded_chars_expanded,
+        W,
+        strides=[1, 1, 1, 1],
+        padding="VALID")
 
-        h_drop = tf.nn.dropout(h_pool_flat, dropout_keep_prob)
-        
-        l2_loss = tf.constant(0.0)
+    h = tf.nn.relu(tf.nn.bias_add(conv, b))
 
-        W = tf.get_variable(
-            "W",
-            shape=[num_filters_total, num_classes],
-            initializer=tf.contrib.layers.xavier_initializer())
-        b = tf.Variable(tf.constant(0.1, shape=[num_classes]))
-        l2_loss += tf.nn.l2_loss(W)
-        l2_loss += tf.nn.l2_loss(b)
-        scores = tf.nn.xw_plus_b(h_drop, W, b)
-        predictions = tf.argmax(scores, 1)
+    pooled = tf.nn.max_pool(
+        h,
+        ksize=[1, sequence_length - filter_size + 1, 1, 1],
+        strides=[1, 1, 1, 1],
+        padding='VALID',
+        name="pool")
+    pooled_outputs.append(pooled)
 
-        losses = tf.nn.softmax_cross_entropy_with_logits(logits=scores,
-                                                         labels=input_y)
-        
-        loss = tf.reduce_mean(losses) + l2_reg_lambda * l2_loss
+num_filters_total = num_filters * len(filter_sizes)
+h_pool = tf.concat(pooled_outputs, 3)
+h_pool_flat = tf.reshape(h_pool, [-1, num_filters_total])
 
-        correct_predictions = tf.equal(predictions, tf.argmax(input_y, 1))
-        accuracy = tf.reduce_mean(tf.cast(correct_predictions, "float"))
+h_drop = tf.nn.dropout(h_pool_flat, dropout_keep_prob)
 
-        global_step = tf.Variable(0, trainable=False)
-        optimizer = tf.train.AdamOptimizer(1e-3)
-        grads_and_vars = optimizer.compute_gradients(loss)
-        train_op = optimizer.apply_gradients(grads_and_vars, global_step=global_step)
+l2_loss = tf.constant(0.0)
 
-        # Keep track of gradient values and sparsity (optional)
-        grad_summaries = []
-        for g, v in grads_and_vars:
-            if g is not None:
-                grad_hist_summary = tf.summary.histogram("{}/grad/hist".format(v.name), g)
-                sparsity_summary = tf.summary.scalar("{}/grad/sparsity".format(v.name), tf.nn.zero_fraction(g))
-                grad_summaries.append(grad_hist_summary)
-                grad_summaries.append(sparsity_summary)
-        grad_summaries_merged = tf.summary.merge(grad_summaries)
+W = tf.get_variable("W",shape=[num_filters_total, num_classes],
+                    initializer=tf.contrib.layers.xavier_initializer())
 
-        sess.run(tf.global_variables_initializer())
+b = tf.Variable(tf.constant(0.1, shape=[num_classes]))
 
-        batches = data_helpers.batch_iter(
-            list(zip(x_train, y_train)), FLAGS.batch_size, FLAGS.num_epochs)
+l2_loss += tf.nn.l2_loss(W)
+l2_loss += tf.nn.l2_loss(b)
 
-        for batch in batches:
-            x_batch, y_batch = zip(*batch)
-            feed_dict = {
-                input_x: x_batch,
-                input_y: y_batch,
-                dropout_keep_prob: FLAGS.dropout_keep_prob
-            }
-            res = sess.run(train_op, feed_dict)
-            res = sess.run(accuracy, feed_dict)
-            print res
+scores = tf.nn.xw_plus_b(h_drop, W, b)
 
+predictions = tf.argmax(scores, 1)
 
+losses = tf.nn.softmax_cross_entropy_with_logits(logits=scores, labels=input_y)
 
+loss = tf.reduce_mean(losses) + l2_reg_lambda * l2_loss
+
+correct_predictions = tf.equal(predictions, tf.argmax(input_y, 1))
+
+accuracy = tf.reduce_mean(tf.cast(correct_predictions, "float"))
+
+global_step = tf.Variable(0, trainable=False)
+
+optimizer = tf.train.AdamOptimizer(1e-3)
+
+grads_and_vars = optimizer.compute_gradients(loss)
+
+train_op = optimizer.apply_gradients(grads_and_vars, global_step=global_step)
+
+sess = tf.Session()
+sess.run(tf.global_variables_initializer())
+
+batches = data_helpers.batch_iter(
+    list(zip(x_train, y_train)), FLAGS.batch_size, FLAGS.num_epochs)
+
+for i,batch in enumerate(batches):
+    x_batch, y_batch = zip(*batch)
+    feed_dict = {
+        input_x: x_batch,
+        input_y: y_batch,
+        dropout_keep_prob: FLAGS.dropout_keep_prob
+    }
+    if (i % 10) == 0:
+        train_acc = sess.run(accuracy, feed_dict)
+        test_acc = sess.run(accuracy, feed_dict)
+        print train_acc, test_acc
+    else:
+        sess.run(train_op, feed_dict)
 
 
 
