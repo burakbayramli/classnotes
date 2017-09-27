a4e2ea6a6a6d49aea8dcb37fc91cfc1d09816ba0
Burak Bayramli <burak@localhost.com>
2017-09-27 16:45:16 +0200

    -

--- algs/nlp/nlp1.py
+++ algs/nlp/nlp1.py
@@ -3,17 +3,15 @@ import tensorflow as tf
 import numpy as np
 import data_helpers
 from tensorflow.contrib import learn
-import constants
 
-FLAGS = tf.flags.FLAGS
-FLAGS._parse_flags()
-print("\nParameters:")
-for attr, value in sorted(FLAGS.__flags.items()):
-    print("{}={}".format(attr.upper(), value))
-print("")
+dev_sample_percentage = .1
+positive_data_file = "./data/rt-polarity.pos"
+negative_data_file = "./data/rt-polarity.neg"
+embedding_dim = 120
+batch_size = 40
+num_epochs = 200
 
-print("Loading data...")
-x_text, y = data_helpers.load_data_and_labels(FLAGS.positive_data_file, FLAGS.negative_data_file)
+x_text, y = data_helpers.load_data_and_labels(positive_data_file, negative_data_file)
 
 max_document_length = max([len(x.split(" ")) for x in x_text])
 vocab_processor = learn.preprocessing.VocabularyProcessor(max_document_length)
@@ -24,7 +22,7 @@ shuffle_indices = np.random.permutation(np.arange(len(y)))
 x_shuffled = x[shuffle_indices]
 y_shuffled = y[shuffle_indices]
 
-dev_sample_index = -1 * int(FLAGS.dev_sample_percentage * float(len(y)))
+dev_sample_index = -1 * int(dev_sample_percentage * float(len(y)))
 x_train, x_dev = x_shuffled[:dev_sample_index], x_shuffled[dev_sample_index:]
 y_train, y_dev = y_shuffled[:dev_sample_index], y_shuffled[dev_sample_index:]
 print("Vocabulary Size: {:d}".format(len(vocab_processor.vocabulary_)))
@@ -34,19 +32,20 @@ tf.reset_default_graph()
 
 num_classes=y_train.shape[1]
 sequence_length=x_train.shape[1]
-num_filters=FLAGS.num_filters
 
 input_x = tf.placeholder(tf.int32, [None, sequence_length])
 input_y = tf.placeholder(tf.float32, [None, num_classes])
 
 W = tf.Variable(tf.random_uniform([len(vocab_processor.vocabulary_),
-                                   FLAGS.embedding_dim], -1.0, 1.0))
+                                   embedding_dim], -1.0, 1.0))
+
 ec = tf.nn.embedding_lookup(W, input_x)
+
 embed = tf.contrib.layers.flatten(ec)
 
 scores = tf.contrib.layers.fully_connected(inputs=embed,
-                                               num_outputs=2, 
-                                               activation_fn=tf.nn.softmax)
+                                           num_outputs=2, 
+                                           activation_fn=tf.nn.softmax)
 
 predictions = tf.argmax(scores, 1)
 
@@ -70,8 +69,9 @@ sess = tf.Session()
 
 sess.run(tf.global_variables_initializer())
 
-batches = data_helpers.batch_iter(
-    list(zip(x_train, y_train)), FLAGS.batch_size, FLAGS.num_epochs)
+batches = data_helpers.batch_iter(list(zip(x_train, y_train)),\
+                                  batch_size,\
+                                  num_epochs)
 
 saver = tf.train.Saver(tf.global_variables())
 
--- algs/nlp/nlp2.py
+++ algs/nlp/nlp2.py
@@ -3,17 +3,19 @@ import tensorflow as tf
 import numpy as np
 import data_helpers
 from tensorflow.contrib import learn
-import constants
 
-FLAGS = tf.flags.FLAGS
-FLAGS._parse_flags()
-print("\nParameters:")
-for attr, value in sorted(FLAGS.__flags.items()):
-    print("{}={}".format(attr.upper(), value))
-print("")
+dev_sample_percentage = .1
+positive_data_file = "./data/rt-polarity.pos"
+negative_data_file = "./data/rt-polarity.neg"
+embedding_dim = 200
+filter_sizes = "3,4,5"
+num_filters = 200
+dropout_keep_prob = 0.5
+l2_reg_lambda = 0.0
+batch_size = 70
+num_epochs = 200
 
-print("Loading data...")
-x_text, y = data_helpers.load_data_and_labels(FLAGS.positive_data_file, FLAGS.negative_data_file)
+x_text, y = data_helpers.load_data_and_labels(positive_data_file, negative_data_file)
 
 max_document_length = max([len(x.split(" ")) for x in x_text])
 vocab_processor = learn.preprocessing.VocabularyProcessor(max_document_length)
@@ -24,7 +26,7 @@ shuffle_indices = np.random.permutation(np.arange(len(y)))
 x_shuffled = x[shuffle_indices]
 y_shuffled = y[shuffle_indices]
 
-dev_sample_index = -1 * int(FLAGS.dev_sample_percentage * float(len(y)))
+dev_sample_index = -1 * int(dev_sample_percentage * float(len(y)))
 x_train, x_dev = x_shuffled[:dev_sample_index], x_shuffled[dev_sample_index:]
 y_train, y_dev = y_shuffled[:dev_sample_index], y_shuffled[dev_sample_index:]
 print("Vocabulary Size: {:d}".format(len(vocab_processor.vocabulary_)))
@@ -34,9 +36,7 @@ tf.reset_default_graph()
 
 num_classes=y_train.shape[1]
 sequence_length=x_train.shape[1]
-num_filters=FLAGS.num_filters
-filter_sizes=list(map(int, FLAGS.filter_sizes.split(",")))
-l2_reg_lambda=FLAGS.l2_reg_lambda
+filter_sizes=list(map(int, filter_sizes.split(",")))
 
 input_x = tf.placeholder(tf.int32, [None, sequence_length])
 input_y = tf.placeholder(tf.float32, [None, num_classes])
@@ -45,14 +45,14 @@ dropout_keep_prob = tf.placeholder(tf.float32)
 l2_loss = tf.constant(0.0)
 
 W = tf.Variable(tf.random_uniform([len(vocab_processor.vocabulary_),
-                                   FLAGS.embedding_dim], -1.0, 1.0))
+                                   embedding_dim], -1.0, 1.0))
 
 embedded_chars = tf.nn.embedding_lookup(W, input_x)
 embedded_chars_expanded = tf.expand_dims(embedded_chars, -1)
 
 pooled_outputs = []
 for i, filter_size in enumerate(filter_sizes):
-    filter_shape = [filter_size, FLAGS.embedding_dim, 1, num_filters]
+    filter_shape = [filter_size, embedding_dim, 1, num_filters]
     W = tf.Variable(tf.truncated_normal(filter_shape, stddev=0.1))
     b = tf.Variable(tf.constant(0.1, shape=[num_filters]))
     conv = tf.nn.conv2d(
@@ -71,7 +71,9 @@ for i, filter_size in enumerate(filter_sizes):
     pooled_outputs.append(pooled)
 
 num_filters_total = num_filters * len(filter_sizes)
+
 h_pool = tf.concat(pooled_outputs, 3)
+
 h_pool_flat = tf.reshape(h_pool, [-1, num_filters_total])
 
 h_drop = tf.nn.dropout(h_pool_flat, dropout_keep_prob)
@@ -110,7 +112,7 @@ sess = tf.Session()
 sess.run(tf.global_variables_initializer())
 
 batches = data_helpers.batch_iter(
-    list(zip(x_train, y_train)), FLAGS.batch_size, FLAGS.num_epochs)
+    list(zip(x_train, y_train)), batch_size, num_epochs)
 
 saver = tf.train.Saver(tf.global_variables())
 
@@ -119,14 +121,14 @@ for i,batch in enumerate(batches):
     feed_dict = {
         input_x: x_batch,
         input_y: y_batch,
-        dropout_keep_prob: FLAGS.dropout_keep_prob
+        dropout_keep_prob: dropout_keep_prob
     }
     sess.run(train_op, feed_dict)
     if (i % 30) == 0:
         feed_dict2 = {
             input_x: x_dev,
             input_y: y_dev,
-            dropout_keep_prob: FLAGS.dropout_keep_prob
+            dropout_keep_prob: dropout_keep_prob
         }
         train_acc = sess.run(accuracy, feed_dict)
         test_acc = sess.run(accuracy, feed_dict2)
