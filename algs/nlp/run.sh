d0568fdba0dc75e3d2521d4d40ecc18cee075e74
Burak Bayramli <burak@localhost.com>
2017-09-27 15:14:43 +0200

    -

--- algs/nlp/run.sh
+++
@@ -1,2 +0,0 @@
-#gcloud compute --project "tensor-rnn-162508" scp --recurse /home/burak/Documents/classnotes/algs/nlp/* instance-1:/home/burak/nlp/ --zone "europe-west1-b" 
-gcloud compute --project "tensor-rnn-162508" scp /home/burak/Documents/classnotes/algs/nlp/*.py instance-1:/home/burak/nlp/ --zone "europe-west1-b" 
