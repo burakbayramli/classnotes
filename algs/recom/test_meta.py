0b33a8bec3c088590fe3a9ba4d468e9fda7cd594
Burak Bayramli <burak@localhost.com>
2017-10-05 09:12:55 +0200

    -

--- algs/recom/test_meta.py
+++ algs/recom/test_meta.py
@@ -8,16 +8,14 @@ import pandas as pd, zipfile
 from sklearn.metrics import mean_squared_error, mean_absolute_error
 
 ML_100K_FOLDER = Path('ml-100k')
-names = ["name", "date", "genre", "url"]
-names += ["f" + str(x) for x in range(19)]  # unused feature names
-
+icols = ["name", "date", "genre", "url"]
+icols += ["f" + str(x) for x in range(19)]  # unused feature names
+rcols  = ["user_id", "item_id", "rating", "timestamp"]
 with zipfile.ZipFile('ml-100k.zip', 'r') as z:
-    all_ratings = pd.read_csv(z.open('ml-100k/u.data'), sep='\t',names=["user_id", "item_id", "rating", "timestamp"])
-    items = pd.read_csv(z.open('ml-100k/u.item'), sep='|', encoding='latin-1',names=names)
+    all_ratings = pd.read_csv(z.open('ml-100k/u.data'), sep='\t', names=rcols)
+    items = pd.read_csv(z.open('ml-100k/u.item'), sep='|',names=icols, encoding='latin-1')
     
-all_ratings.head()
 items.fillna(value="01-Jan-1997", inplace=True)
-items.head()
 
 from sklearn.model_selection import train_test_split
 
@@ -51,21 +49,19 @@ item_meta_test = items["scaled_date"][item_id_test]
 
 # For each sample we input the integer identifiers
 # of a single user and a single item
-user_id_input = Input(shape=[1], name='user')
-item_id_input = Input(shape=[1], name='item')
-meta_input = Input(shape=[1], name='meta_item')
+user_id_input = Input(shape=[1])
+item_id_input = Input(shape=[1])
+meta_input = Input(shape=[1])
 
 embedding_size = 32
 user_embedding = Embedding(output_dim=embedding_size, input_dim=max_user_id + 1,
-                           input_length=1, name='user_embedding')(user_id_input)
+                           input_length=1)(user_id_input)
 item_embedding = Embedding(output_dim=embedding_size, input_dim=max_item_id + 1,
-                           input_length=1, name='item_embedding')(item_id_input)
+                           input_length=1)(item_id_input)
 
 
-# reshape from shape: (batch_size, input_length, embedding_size)
-# to shape: (batch_size, input_length * embedding_size) which is
-# equal to shape: (batch_size, embedding_size)
 user_vecs = Flatten()(user_embedding)
+
 item_vecs = Flatten()(item_embedding)
 
 input_vecs = merge([user_vecs, item_vecs, meta_input], mode='concat')
