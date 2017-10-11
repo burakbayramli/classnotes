755d7e0b6d0f3bc8ca2a6e402c7475c42dd59deb
Burak Bayramli <burak@localhost.com>
2017-10-11 14:01:08 +0200

    -

---
+++ algs/seq/seq.py
@@ -0,0 +1,9 @@
+import data_utils
+
+data_utils.prepare_data('/home/burak/Downloads/', 
+                        '/home/burak/Downloads/tur-eng/tatoeba-eng.txt', 
+                        '/home/burak/Downloads/tur-eng/tatoeba-tur.txt', 
+                        '/home/burak/Downloads/tur-eng/dev-eng.txt',
+                        '/home/burak/Downloads/tur-eng/dev-tur.txt',
+                        10000,
+                        10000)
