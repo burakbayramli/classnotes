a8162f8dbd56fcfdde3e499950c05a498a35968e
Burak Bayramli <burak@localhost.com>
2017-10-16 14:58:01 +0200

    -

--- build.py
+++ build.py
@@ -8,12 +8,25 @@ cfg = """
 \\EndPreamble
 """
 
+ad = '''<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+<!-- htlatex -->
+<ins class="adsbygoogle"
+     style="display:inline-block;width:728px;height:90px"
+     data-ad-client="ca-pub-1121997141902625"
+     data-ad-slot="1808738179"></ins>
+<script>
+(adsbygoogle = window.adsbygoogle || []).push({});
+</script>
+'''
+
 def translit_low(c):
     res = c.lower()
     res = res.replace(u'ğ','g')
     res = res.replace(u'ş','s')
     res = res.replace(u'ı','i')
     res = res.replace(u'ç','c')
+    res = res.replace(u'ü','u')
+    res = res.replace(u'ö','o')
     return res
 
 if __name__ == "__main__": 
@@ -38,10 +51,19 @@ if __name__ == "__main__":
                 os.chdir("..")
 
     if sys.argv[1] == 'html':
-        tgt = "/home/burak/Downloads/classnotes_html"
-        #cmd = "python /home/burak/Documents/kod/rsync.py '%s' '%s' --delete" % (os.getcwd(), tgt)
-        #print cmd
-        #os.system(cmd)
+        
+        fr = os.getcwd() + "/static_files"
+        tgt = "/home/burak/Downloads/classnotes_html/static_files"
+        cmd = "python /home/burak/Documents/kod/rsync.py '%s' '%s'" % (fr, tgt)
+        print cmd
+        os.system(cmd)
+
+        fr = os.getcwd()
+        tgt = tgt + "/static/classnotes"
+        cmd = "python /home/burak/Documents/kod/rsync.py '%s' '%s'" % (fr, tgt)
+        print cmd
+        os.system(cmd)
+        
         for topdir in ['algs','calc_multi','chaos','compscieng',
                        'func_analysis','linear','ode','pde','stat',
                        'tser','vision']:
@@ -56,7 +78,6 @@ if __name__ == "__main__":
                 # read tex file, get header
                 fin = open(dir + "/" + subdir + "/" + subdir + ".tex")
                 content = fin.read()
-                #print content
                 title = re.findall(u"begin.*?document.*?\n(.*?)\n",content.decode('latin5'),re.DOTALL)[0]
                 url = translit_low(title)
                 url = url.replace(" ","_")
@@ -70,36 +91,23 @@ if __name__ == "__main__":
                 fout.write("\n")
                 fin.close()
 
-                os.chdir(dir + "/" + subdir)                
+                print 'chdir', dir + "/" + subdir
+                os.chdir(dir + "/" + subdir)
                 ocfg = open(subdir + ".cfg", "w")
                 ocfg.write(cfg)
                 ocfg.close()
                 cmd = 'htlatex %s.tex "%s" "" "" -shell-escape' % (subdir,subdir)     
                 os.system(cmd)
-                
+
+                # read 16 lines, insert ad, then continue
+                fin1 = open(subdir + ".html")
+                fout1 = codecs.open(url, "w")
+                for i,line in enumerate(fin1.readlines()):
+                    fout1.write(line)
+                    if i == 15:  fout1.write(ad)
+                fout1.close()                
                 break
             fout.write("</html>\n")
             fout.close()
             break
-
-
-        # for root, directories, filenames in os.walk(tgt):
-        #     print root, directories, filenames
-        #     for filename in filenames:
-        #         print filename
-        #         path = os.path.join(root,filename)
-        #         if ".tex" in path:
-        #             dir = os.path.dirname(os.path.abspath(path))
-        #             if "00" in dir: continue
-        #             base = os.path.basename(path).replace(".tex","")
-        #             print dir, base
-        #             os.chdir(dir)
-        #             out = open(base + ".cfg", "w")
-        #             out.write(cfg)
-        #             out.close()
-        #             cmd = 'htlatex %s.tex "%s" "" "" -shell-escape' % (base,base)     
-        #             print cmd
-        #             os.system(cmd)
-        #             break
-        #         break
-            
+                      
