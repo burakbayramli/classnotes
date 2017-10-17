cd69966a9c0b01a3e84bbd8dd2b6427f7a9805fd
Burak Bayramli <burak@localhost.com>
2017-10-17 10:49:27 +0200

    -

--- build.py
+++ build.py
@@ -85,6 +85,7 @@ if __name__ == "__main__":
                 url = url.replace(" ","_")
                 url = url.replace("(","_")
                 url = url.replace(")","_")
+                url = url.replace("/","_")
                 url = url.replace("-","")
                 url = url.replace(",","")
                 url = url + ".html"
