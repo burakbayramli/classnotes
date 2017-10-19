c6b51a3944709df2b08a7918cfce988d9128667c
Burak Bayramli <burak@localhost.com>
2017-10-19 10:39:44 +0200

    -

--- algs/churn/k3.py
+++ algs/churn/k3.py
@@ -22,16 +22,6 @@ def _keras_unstack_hack(ab):
         b = ab[..., 1]
     return a, b
 
-
-def weibull_loss_discrete(y_true, y_pred, name=None):
-    y,u = _keras_unstack_hack(y_true)
-    a,b = _keras_unstack_hack(y_pred)
-    hazard0 = K.pow((y + 1e-35) / a, b)
-    hazard1 = K.pow((y + 1.0) / a, b)    
-    loglikelihoods = u * K.log(K.exp(hazard1 - hazard0) - 1.0) - hazard1
-    loss = -1 * K.mean(loglikelihoods)
-    return loss
-
 def output_lambda(x, init_alpha=1.0, max_beta_value=5.0, max_alpha_value=None):
     a, b = _keras_unstack_hack(x)
     if max_alpha_value is None:
@@ -50,6 +40,16 @@ def output_lambda(x, init_alpha=1.0, max_beta_value=5.0, max_alpha_value=None):
     b = m * K.clip(x=b, min_value=K.epsilon(), max_value=1. - K.epsilon())
     x = K.stack([a, b], axis=-1)
     return x
+
+
+def weibull_loss_discrete(y_true, y_pred, name=None):
+    y,u = _keras_unstack_hack(y_true)
+    a,b = _keras_unstack_hack(y_pred)
+    hazard0 = K.pow((y + 1e-35) / a, b)
+    hazard1 = K.pow((y + 1.0) / a, b)    
+    loglikelihoods = u * K.log(K.exp(hazard1 - hazard0) - 1.0) - hazard1
+    loss = -1 * K.mean(loglikelihoods)
+    return loss
 
 n_timesteps    = 200
 n_sequences = every_nth = 80
@@ -80,7 +80,7 @@ model.compile(loss=weibull_loss_discrete, optimizer=adam(lr=.01))
 model.summary()
 
 np.random.seed(1)
-model.fit(x_train, y_train, epochs=80,  batch_size=x_train.shape[0]/10, 
+model.fit(x_train, y_train, epochs=2,  batch_size=x_train.shape[0]/10, 
           verbose=2,  validation_data=(x_test, y_test), callbacks=[history])
 
-model.save("/tmp/wtte-simple")
+model.save_weights("wtte-simple-weights.h5")
