ea0a148ce5d825f6f86ddba2ccda7a790d0c1935
Burak Bayramli <burak@localhost.com>
2017-10-25 09:53:11 +0200

    -

--- algs/churn/churn7.py
+++ algs/churn/churn7.py
@@ -16,28 +16,28 @@ file.close()
 res = []
 N = 100
 for i in range(len(recs)):
-    if len(recs[i]) > N:res.append(recs[i][-N:])
+    if len(recs[i]) > N: res.append(recs[i][-N:])
 res = np.array(res).reshape(len(res),N,5)
 print res.shape
 
 x = res[:,:,[2,3]]
 y = res[:,:,[0,1]]
-print x.shape, y.shape
+print 'x',x.shape, 'y',y.shape
 
-B = 2000
+B = 1200
 x_train = x[0:B]; y_train = y[0:B]
 x_test = x[B:];   y_test = y[B:]
 
-print x_train.shape
+print 'train', x_train.shape, 'test', x_test.shape
 
 np.random.seed(1)
 
-args = {"init_alpha":30., "max_beta_value":4.0}
+args = {"init_alpha":5., "max_beta_value":4.0}
 
 history = History()
 model = Sequential()
 model.add( LSTM(2, input_shape=(N, 2), activation='relu', return_sequences=True) )
-model.add( Dense(30) )
+model.add( Dense(2) )
 model.add( Lambda(util.output_lambda, arguments=args ))
 model.compile(loss=util.weibull_loss_discrete, optimizer=adam(lr=.01))
 
@@ -46,7 +46,9 @@ model.summary()
 np.random.seed(1)
 model.fit(x_train, y_train,
           epochs=80,
-          batch_size=200, 
+          batch_size=500, 
           verbose=2,
           validation_data=(x_test, y_test),
           callbacks=[history])
+
+model.save_weights("wtte-retail-weights.h5")
