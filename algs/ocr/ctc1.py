1292991e85032cc887481c100839eb5bade71ecc
Burak Bayramli <burak@localhost.com>
2017-11-17 14:14:13 +0100

    -

--- algs/ocr/ctc1.py
+++ algs/ocr/ctc1.py
@@ -1,22 +1,5 @@
-# Copyright 2016 The TensorFlow Authors. All Rights Reserved.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-# ==============================================================================
-"""Tests for tensorflow.ctc_ops.ctc_decoder_ops."""
-
 from __future__ import absolute_import
 from __future__ import division
-from __future__ import print_function
 
 import numpy as np
 
@@ -25,16 +8,13 @@ from tensorflow.python.framework import dtypes
 from tensorflow.python.framework import errors_impl
 from tensorflow.python.framework import sparse_tensor
 from tensorflow.python.ops import ctc_ops
-from tensorflow.python.ops import gradients_impl
 from tensorflow.python.platform import test
 
 
 def SimpleSparseTensorFrom(x):
   """Create a very simple SparseTensor with dimensions (batch, time).
-
   Args:
     x: a list of lists of type int
-
   Returns:
     x_ix and x_val, the indices and values of the SparseTensor<2>.
   """
@@ -48,87 +28,13 @@ def SimpleSparseTensorFrom(x):
   x_ix = constant_op.constant(x_ix, dtypes.int64)
   x_val = constant_op.constant(x_val, dtypes.int32)
   x_shape = constant_op.constant(x_shape, dtypes.int64)
-
   return sparse_tensor.SparseTensor(x_ix, x_val, x_shape)
 
 
 class CTCLossTest(test.TestCase):
 
-  def _testCTCLoss(self,
-                   inputs,
-                   seq_lens,
-                   labels,
-                   expected_err_re=None):
-
-    inputs_t = constant_op.constant(inputs)
-
-    with self.test_session(use_gpu=False) as sess:
-      loss = ctc_ops.ctc_loss(
-          inputs=inputs_t, labels=labels, sequence_length=seq_lens)
-      grad = gradients_impl.gradients(loss, [inputs_t])[0]
-
-      print (loss.eval())
-
   def testBasic(self):
-    """Test two batch entries."""
     # Input and ground truth from Alex Graves' implementation.
-    #
-    #### Batch entry 0 #####
-    # targets: 0 1 2 1 0
-    # outputs:
-    # 0 0.633766 0.221185 0.0917319 0.0129757 0.0142857 0.0260553
-    # 1 0.111121 0.588392 0.278779 0.0055756 0.00569609 0.010436
-    # 2 0.0357786 0.633813 0.321418 0.00249248 0.00272882 0.0037688
-    # 3 0.0663296 0.643849 0.280111 0.00283995 0.0035545 0.00331533
-    # 4 0.458235 0.396634 0.123377 0.00648837 0.00903441 0.00623107
-    # alpha:
-    # 0 -3.64753 -0.456075 -inf -inf -inf -inf -inf -inf -inf -inf -inf
-    # 1 -inf -inf -inf -0.986437 -inf -inf -inf -inf -inf -inf -inf
-    # 2 -inf -inf -inf -inf -inf -2.12145 -inf -inf -inf -inf -inf
-    # 3 -inf -inf -inf -inf -inf -inf -inf -2.56174 -inf -inf -inf
-    # 4 -inf -inf -inf -inf -inf -inf -inf -inf -inf -3.34211 -inf
-    # beta:
-    # 0 -inf -2.88604 -inf -inf -inf -inf -inf -inf -inf -inf -inf
-    # 1 -inf -inf -inf -2.35568 -inf -inf -inf -inf -inf -inf -inf
-    # 2 -inf -inf -inf -inf -inf -1.22066 -inf -inf -inf -inf -inf
-    # 3 -inf -inf -inf -inf -inf -inf -inf -0.780373 -inf -inf -inf
-    # 4 -inf -inf -inf -inf -inf -inf -inf -inf -inf 0 0
-    # prob: -3.34211
-    # outputDerivs:
-    # 0 -0.366234 0.221185 0.0917319 0.0129757 0.0142857 0.0260553
-    # 1 0.111121 -0.411608 0.278779 0.0055756 0.00569609 0.010436
-    # 2 0.0357786 0.633813 -0.678582 0.00249248 0.00272882 0.0037688
-    # 3 0.0663296 -0.356151 0.280111 0.00283995 0.0035545 0.00331533
-    # 4 -0.541765 0.396634 0.123377 0.00648837 0.00903441 0.00623107
-    #
-    #### Batch entry 1 #####
-    #
-    # targets: 0 1 1 0
-    # outputs:
-    # 0 0.30176 0.28562 0.0831517 0.0862751 0.0816851 0.161508
-    # 1 0.24082 0.397533 0.0557226 0.0546814 0.0557528 0.19549
-    # 2 0.230246 0.450868 0.0389607 0.038309 0.0391602 0.202456
-    # 3 0.280884 0.429522 0.0326593 0.0339046 0.0326856 0.190345
-    # 4 0.423286 0.315517 0.0338439 0.0393744 0.0339315 0.154046
-    # alpha:
-    # 0 -1.8232 -1.19812 -inf -inf -inf -inf -inf -inf -inf
-    # 1 -inf -2.19315 -2.83037 -2.1206 -inf -inf -inf -inf -inf
-    # 2 -inf -inf -inf -2.03268 -3.71783 -inf -inf -inf -inf
-    # 3 -inf -inf -inf -inf -inf -4.56292 -inf -inf -inf
-    # 4 -inf -inf -inf -inf -inf -inf -inf -5.42262 -inf
-    # beta:
-    # 0 -inf -4.2245 -inf -inf -inf -inf -inf -inf -inf
-    # 1 -inf -inf -inf -3.30202 -inf -inf -inf -inf -inf
-    # 2 -inf -inf -inf -inf -1.70479 -0.856738 -inf -inf -inf
-    # 3 -inf -inf -inf -inf -inf -0.859706 -0.859706 -0.549337 -inf
-    # 4 -inf -inf -inf -inf -inf -inf -inf 0 0
-    # prob: -5.42262
-    # outputDerivs:
-    # 0 -0.69824 0.28562 0.0831517 0.0862751 0.0816851 0.161508
-    # 1 0.24082 -0.602467 0.0557226 0.0546814 0.0557528 0.19549
-    # 2 0.230246 0.450868 0.0389607 0.038309 0.0391602 -0.797544
-    # 3 0.280884 -0.570478 0.0326593 0.0339046 0.0326856 0.190345
-    # 4 -0.576714 0.315517 0.0338439 0.0393744 0.0339315 0.154046
 
     # max_time_steps == 7
     depth = 6
@@ -166,17 +72,26 @@ class CTCLossTest(test.TestCase):
             [input_log_prob_matrix_0[t, :], input_log_prob_matrix_1[t, :]])
         for t in range(5)
     ] + 2 * [np.nan * np.ones((2, depth), np.float32)]
-
+    
     # convert inputs into [max_time x batch_size x depth tensor] Tensor
     inputs = np.asarray(inputs, dtype=np.float32)
+    print inputs.shape
     
     # len batch_size array of label vectors
+    print targets_0
     labels = SimpleSparseTensorFrom([targets_0, targets_1])
 
     # batch_size length vector of sequence_lengths
     seq_lens = np.array([5, 5], dtype=np.int32)
 
-    self._testCTCLoss(inputs, seq_lens, labels)
+    #self._testCTCLoss(inputs, seq_lens, labels)
+    inputs_t = constant_op.constant(inputs)
+
+    with self.test_session(use_gpu=False) as sess:
+      loss = ctc_ops.ctc_loss(
+          inputs=inputs_t, labels=labels, sequence_length=seq_lens)
+      print (loss.eval())
+
 
 if __name__ == "__main__":
   test.main()
