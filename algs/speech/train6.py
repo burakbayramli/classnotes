426abbb675d37cf83468a874acf5eefcb1dfc482
Burak Bayramli <burak@localhost.com>
2017-12-09 02:13:14 +0100

    -

--- algs/speech/train6.py
+++ algs/speech/train6.py
@@ -7,11 +7,11 @@ from glob import glob
 import time, re, os, random
 import numpy as np
 
+random.seed(0)
 num_units = 200
 num_layers = 3
-batch_size = 5
+batch_size = 2
 num_epochs = 1000
-num_batches_per_epoch = 10
 sample_rate=16000
 num_features = 26
 steps_before_after = 9
@@ -19,26 +19,15 @@ SPACE_TOKEN = '<space>'
 SPACE_INDEX = 0
 FIRST_INDEX = ord('a') - 1  # 0 is reserved to space
 mfile = "/tmp/speech.ckpt"
+labels = ['down','go','left','no','off','on','right','stop','up','yes']
 
 def load_wavfile(wavfile):
-    """
-    Read a wav file using scipy.io.wavfile
-    """
+
     rate, sig = wav.read(wavfile)
     data_name = os.path.splitext(os.path.basename(wavfile))[0]
     return rate, sig, data_name
 
 def audiofile_to_input_vector(audio_filename, numcep, numcontext):
-    '''
-    Turn an audio file into feature representation.
-
-    This function has been modified from Mozilla DeepSpeech:
-    https://github.com/mozilla/DeepSpeech/blob/master/util/audio.py
-
-    # This Source Code Form is subject to the terms of the Mozilla Public
-    # License, v. 2.0. If a copy of the MPL was not distributed with this
-    # file, You can obtain one at http://mozilla.org/MPL/2.0/.
-    '''
 
     # Load wav files
     fs, audio = wav.read(audio_filename)
@@ -46,35 +35,15 @@ def audiofile_to_input_vector(audio_filename, numcep, numcontext):
     # Get mfcc coefficients
     orig_inputs = mfcc(audio, samplerate=fs, numcep=numcep)
 
-    # We only keep every second feature (BiRNN stride = 2)
     orig_inputs = orig_inputs[::2]
-
-    # For each time slice of the training set, we need to copy the context this makes
-    # the numcep dimensions vector into a numcep + 2*numcep*numcontext dimensions
-    # because of:
-    #  - numcep dimensions for the current mfcc feature set
-    #  - numcontext*numcep dimensions for each of the past and future (x2) mfcc feature set
-    # => so numcep + 2*numcontext*numcep
     train_inputs = np.array([], np.float32)
     train_inputs.resize((orig_inputs.shape[0], numcep + 2 * numcep * numcontext))
-
-    # Prepare pre-fix post fix context
     empty_mfcc = np.array([])
     empty_mfcc.resize((numcep))
-
-    # Prepare train_inputs with past and future contexts
     time_slices = range(train_inputs.shape[0])
     context_past_min = time_slices[0] + numcontext
     context_future_max = time_slices[-1] - numcontext
     for time_slice in time_slices:
-        # Reminder: array[start:stop:step]
-        # slices from indice |start| up to |stop| (not included), every |step|
-
-        # Add empty context data of the correct size to the start and end
-        # of the MFCC feature matrix
-
-        # Pick up to numcontext time slices in the past, and complete with empty
-        # mfcc features
         need_empty_past = max(0, (context_past_min - time_slice))
         empty_source_past = list(empty_mfcc for empty_slots in range(need_empty_past))
         data_source_past = orig_inputs[max(0, time_slice - numcontext):time_slice]
@@ -103,14 +72,10 @@ def audiofile_to_input_vector(audio_filename, numcep, numcontext):
 
         train_inputs[time_slice] = np.concatenate((past, now, future))
         assert(len(train_inputs[time_slice]) == numcep + 2 * numcep * numcontext)
-
-    # Scale/standardize the inputs
-    # This can be done more efficiently in the TensorFlow graph
+        
     train_inputs = (train_inputs - np.mean(train_inputs)) / np.std(train_inputs)
     return train_inputs
     
-fdir = "/home/burak/Downloads/train/audio"
-
 def find_files(directory, pattern='.wav'):
     """Recursively finds all files matching the pattern."""
     files = []
@@ -122,81 +87,58 @@ def find_files(directory, pattern='.wav'):
     return res
 
 audio_files = find_files("/home/burak/Downloads/goog_tf_speech/audio")
-print len(audio_files)
 
-f = '/home/burak/Downloads/goog_tf_speech/audio/down/00176480_nohash_0.wav'
-res = audiofile_to_input_vector(f, num_features, steps_before_after)
-res = res.astype('float32')
-print res.shape
-
-f = '/home/burak/Downloads/goog_tf_speech/audio/down/00176480_nohash_0.wav'
-res = audiofile_to_input_vector(f, num_features, steps_before_after)
-res = res.astype('float32')
-print res.shape
-
-labels = ['down','go','left','no','off','on','right','stop','up','yes']
-
-random.seed(0)
 def get_minibatch(batch_size):
-    res = []
+    res = np.zeros((batch_size, 50, 494))
     y = np.zeros((batch_size,len(labels)))
-    for i in range(batch_size):
+    i = 0
+    while True:
     	f = random.choice(audio_files)
 	a = audiofile_to_input_vector(f, num_features, steps_before_after)
 	a = a.astype('float32')
-    	res.append(a)
+        if a.shape[0] != 50: continue
+    	res[i, :, :] = a
 	label = re.findall(".*/(.*?)/.*?.wav",f)[0]
+        print label
 	y[i, labels.index(label)] = 1.0
-    res = np.array(res)
-    y = np.array(y)
+        i += 1
+        if i==batch_size: break
+
     return res, y
 
-data,y = get_minibatch(1)
-print data.shape, y.shape
-print y[0]
+if __name__ == "__main__":
 
-tf.reset_default_graph()
+    tf.reset_default_graph()
 
-# e.g: log filter bank or MFCC features
-# Has size [batch_size, max_step_size, num_features], but the
-# batch_size and max_step_size can vary along each step
-X = tf.placeholder(tf.float32, [batch_size, 50, 494])
-y = tf.placeholder(tf.float32, shape=[batch_size, len(labels)])
+    X = tf.placeholder(tf.float32, [batch_size, 50, 494])
+    y = tf.placeholder(tf.float32, shape=[batch_size, len(labels)])
 
-basic_cell = tf.contrib.rnn.GRUCell(num_units=num_units)
-multi_layer_cell = tf.contrib.rnn.MultiRNNCell([basic_cell] * num_units)
-cells = []
-for _ in range(num_layers):
-    cell = tf.contrib.rnn.LSTMCell(num_units) 
-    cells.append(cell)
-cell = tf.contrib.rnn.MultiRNNCell(cells)
-output, states = tf.nn.dynamic_rnn(cell, X, dtype=tf.float32)
-last = states[-1][0]
-logits = tf.contrib.layers.fully_connected(inputs=last,
-                                           num_outputs=len(labels),
-					   activation_fn=None)
+    cells = []
+    for _ in range(num_layers):
+        cell = tf.contrib.rnn.LSTMCell(num_units) 
+        cells.append(cell)
+    cell = tf.contrib.rnn.MultiRNNCell(cells)
+    output, states = tf.nn.dynamic_rnn(cell, X, dtype=tf.float32)
+    last = states[-1][0]
+    logits = tf.contrib.layers.fully_connected(inputs=last,
+                                               num_outputs=len(labels),
+                                               activation_fn=None)
 
-print logits
+    softmax = tf.nn.softmax_cross_entropy_with_logits(logits=logits,labels=y) 
 
-softmax = tf.nn.softmax_cross_entropy_with_logits(logits=logits,labels=y)                         
+    cross_entropy = tf.reduce_mean(softmax)
+    train_step = tf.train.AdamOptimizer(0.001).minimize(cross_entropy)
 
-cross_entropy = tf.reduce_mean(softmax)
-train_step = tf.train.AdamOptimizer(0.001).minimize(cross_entropy)
+    correct_prediction = tf.equal(tf.argmax(y,1), tf.argmax(logits,1))
+    accuracy = (tf.reduce_mean(tf.cast(correct_prediction, tf.float32)))*100.
 
-correct_prediction = tf.equal(tf.argmax(y,1), tf.argmax(logits,1))
-accuracy = (tf.reduce_mean(tf.cast(correct_prediction, tf.float32)))*100
-
-with tf.Session() as sess:
-    sess.run(tf.global_variables_initializer())
-    for i in range(num_epochs):
-        print i
-        try:
+    with tf.Session() as sess:
+        sess.run(tf.global_variables_initializer())
+        for i in range(num_epochs):
+            print i
             x_batch, y_batch = get_minibatch(batch_size)
             sess.run(train_step,feed_dict={X:x_batch, y:y_batch})
             if i % 10 == 0: 
                 acc = sess.run(accuracy,feed_dict={X:x_batch, y:y_batch})
                 print 'accuracy', acc
-        except:
-            #print 'error'
-            continue
-               
+
