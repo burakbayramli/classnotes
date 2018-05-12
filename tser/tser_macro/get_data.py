e08779dc78ffd2f2416adbc24a86458db8de1c28
Burak Bayramli <burak@localhost.com>
2018-05-12 21:20:13 +0200

    -

--- tser/tser_macro/get_data.py
+++ tser/tser_macro/get_data.py
@@ -2,12 +2,14 @@ import pandas as pd, datetime
 from pandas_datareader import data
 start=datetime.datetime(1970, 1, 1)
 end=datetime.datetime(2017, 1, 1)
-# Personal Consumption Expenditures - PCE
+# Personal Consumption Expenditures - DPCERD3Q086SBEA
 # Federal government total expenditures - W019RCQ027SBEA
+# State local government total expenditures -  W079RCQ027SBEA
 # Real Gross Private Domestic Investment - GPDIC1
 # Net Exports of Goods and Services - NETEXP
-# Real Gross Domestic Product - GDPC1
+# Gross Domestic Product - GDP
 # Total Credit to Private Non-Financial Sector - CRDQUSAPABIS
-df = data.DataReader(['PCE','W019RCQ027SBEA','GPDIC1','NETEXP','GDPC1','CRDQUSAPABIS'], 'fred', start, end)
-df.columns = ['consump','govexp','invest','netexp','gdp','crt']
+# Real Estate Loans, All Commercial Banks - REALLN
+df = data.DataReader(['DPCERD3Q086SBEA','W019RCQ027SBEA','W079RCQ027SBEA','GPDIC1','NETEXP','GDP','CRDQUSAPABIS','REALLN'], 'fred', start, end)
+df.columns = ['consump','gov1exp','gov2exp', 'invest','netexp','gdp','nonfinloan','constructloan']
 df.to_csv('data.csv')
