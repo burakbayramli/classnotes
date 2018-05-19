8df4b5ae0faf8faf57abc18d886c7907dee3bdd4
Burak Bayramli <burak@localhost.com>
2018-05-19 23:50:08 +0200

    -

--- tser/tser_macro/get_data.py
+++ tser/tser_macro/get_data.py
@@ -2,6 +2,18 @@ import pandas as pd, datetime
 from pandas_datareader import data
 start=datetime.datetime(1950, 1, 1)
 end=datetime.datetime(2017, 1, 1)
+#df = data.DataReader(['M2SL','GDP','CIBOARD','REALLN'], 'fred', start, end)
+#df = data.DataReader(['M2SL','GDP','BCNSDODNS','REALLN'], 'fred', start, end)
+df = data.DataReader(['M2SL','GDP','BCNSDODNS','CLDACBM027NBOG'], 'fred', start, end)
+df.columns = ['m2cd','gdp','cred','const']
+df.to_csv('data2.csv')
+
+exit()
+
+import pandas as pd, datetime
+from pandas_datareader import data
+start=datetime.datetime(1950, 1, 1)
+end=datetime.datetime(2017, 1, 1)
 # Personal Consumption Expenditures - DPCERD3Q086SBEA
 # Federal government total expenditures - W019RCQ027SBEA
 # State local government total expenditures -  W079RCQ027SBEA
