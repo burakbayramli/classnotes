63d7ad76f76294dee9fa3dcdf6aa95c469eac481
Burak Bayramli <burak@localhost.com>
2018-05-20 20:48:58 +0200

    -

--- tser/tser_macro/get_data.py
+++ tser/tser_macro/get_data.py
@@ -1,18 +1,15 @@
 import pandas as pd, datetime
 from pandas_datareader import data
-start=datetime.datetime(1950, 1, 1)
+start=datetime.datetime(1970, 1, 1)
 end=datetime.datetime(2017, 1, 1)
-#df = data.DataReader(['M2SL','GDP','CIBOARD','REALLN'], 'fred', start, end)
-#df = data.DataReader(['M2SL','GDP','BCNSDODNS','REALLN'], 'fred', start, end)
-df = data.DataReader(['M2SL','GDP','BCNSDODNS','CLDACBM027NBOG'], 'fred', start, end)
-df.columns = ['m2cd','gdp','cred','const']
+df = data.DataReader(['M2SL','GDP','CRDQUSAPABIS','REALLN'], 'fred', start, end)
+#df = data.DataReader(['M2SL','GDP','BUSLOANS'], 'fred', start, end)
+df.columns = ['m2cd','gdp','nonfincred','realest']
 df.to_csv('data2.csv')
-
-#exit()
 
 import pandas as pd, datetime
 from pandas_datareader import data
-start=datetime.datetime(1950, 1, 1)
+start=datetime.datetime(1970, 1, 1)
 end=datetime.datetime(2017, 1, 1)
 # Personal Consumption Expenditures - DPCERD3Q086SBEA
 # Federal government total expenditures - W019RCQ027SBEA
@@ -22,6 +19,6 @@ end=datetime.datetime(2017, 1, 1)
 # Gross Domestic Product - GDP
 # Total Credit to Private Non-Financial Sector - CRDQUSAPABIS
 # Real Estate Loans, All Commercial Banks - REALLN
-df = data.DataReader(['DPCERD3Q086SBEA','W019RCQ027SBEA','W079RCQ027SBEA','GPDIC1','NETEXP','GDP','BCNSDODNS','REALLN'], 'fred', start, end)
+df = data.DataReader(['DPCERD3Q086SBEA','W019RCQ027SBEA','W079RCQ027SBEA','GPDIC1','NETEXP','GDP','CRDQUSAPABIS','REALLN'], 'fred', start, end)
 df.columns = ['consump','gov1exp','gov2exp', 'invest','netexp','gdp','nonfinloan','constructloan']
 df.to_csv('data.csv')
