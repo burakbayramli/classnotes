ae98d2d37474a4ec99f9c49ccc29382fa9e3a2d2
Burak Bayramli <burak@localhost.com>
2018-05-20 18:41:49 +0200

    -

--- tser/tser_macro/get_data.py
+++ tser/tser_macro/get_data.py
@@ -8,7 +8,7 @@ df = data.DataReader(['M2SL','GDP','BCNSDODNS','CLDACBM027NBOG'], 'fred', start,
 df.columns = ['m2cd','gdp','cred','const']
 df.to_csv('data2.csv')
 
-exit()
+#exit()
 
 import pandas as pd, datetime
 from pandas_datareader import data
@@ -22,6 +22,6 @@ end=datetime.datetime(2017, 1, 1)
 # Gross Domestic Product - GDP
 # Total Credit to Private Non-Financial Sector - CRDQUSAPABIS
 # Real Estate Loans, All Commercial Banks - REALLN
-df = data.DataReader(['DPCERD3Q086SBEA','W019RCQ027SBEA','W079RCQ027SBEA','GPDIC1','NETEXP','GDP','CRDQUSAPABIS','REALLN'], 'fred', start, end)
+df = data.DataReader(['DPCERD3Q086SBEA','W019RCQ027SBEA','W079RCQ027SBEA','GPDIC1','NETEXP','GDP','BCNSDODNS','REALLN'], 'fred', start, end)
 df.columns = ['consump','gov1exp','gov2exp', 'invest','netexp','gdp','nonfinloan','constructloan']
 df.to_csv('data.csv')
