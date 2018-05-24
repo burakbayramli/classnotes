3c9d94763933f0af9e2771715c5475d04c0ce46d
Burak Bayramli <burak@localhost.com>
2018-05-24 22:45:10 +0200

    -

--- tser/tser_macro/get_data.py
+++ tser/tser_macro/get_data.py
@@ -6,8 +6,6 @@ df = data.DataReader(['GDP','IR3TIB01USM156N'], 'fred', start, end)
 df.columns = ['gdp','shortrate']
 df.to_csv('rates.csv')
 
-exit()
-
 import pandas as pd, datetime
 from pandas_datareader import data
 start=datetime.datetime(1970, 1, 1)
@@ -31,3 +29,21 @@ end=datetime.datetime(2017, 1, 1)
 df = data.DataReader(['DPCERD3Q086SBEA','W019RCQ027SBEA','W079RCQ027SBEA','GPDIC1','NETEXP','GDP','CRDQUSAPABIS','REALLN'], 'fred', start, end)
 df.columns = ['consump','gov1exp','gov2exp', 'invest','netexp','gdp','nonfinloan','constructloan']
 df.to_csv('crowd.csv')
+
+#Total Borrowings of Depository Institutions from the Federal Reserve
+#BORROW
+#Central Bank Assets to GDP for Japan
+#DDDI06JPA156NWDB
+#Gross Domestic Product for Japan
+#JPNNGDP
+
+import pandas as pd, datetime
+from pandas_datareader import data
+start=datetime.datetime(1970, 1, 1)
+end=datetime.datetime(2017, 1, 1)
+df = data.DataReader(['BORROW','DDDI06JPA156NWDB','JPNNGDP','EXJPUS'], 'fred', start, end)
+df.columns = ['us','DDDI06JPA156NWDB','JPNNGDP','curr']
+df.to_csv('curr.csv')
+
+# http://wwe.economagic.com/em-cgi/data.exe/bjap/hms11
+# get this by copy and paste, into bojliq.csv
