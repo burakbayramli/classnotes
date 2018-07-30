b842020043d2d332fd02644c647dd6f13bbfbc01
Burak Bayramli <burak@localhost.com>
2018-07-30 22:06:32 +0200

    -

---
+++ elecmag/build.py
@@ -0,0 +1,24 @@
+import os, sys, glob
+
+pdfs = " ".join(sorted(list(glob.glob('./*/*.pdf'))))
+
+if len(sys.argv) == 1:
+    os.system("pdftk %s output ../../Dropbox/Public/skfiles/elecmag.pdf" % pdfs)
+    exit()    
+elif sys.argv[1] == 'all':
+    for a in sorted(glob.glob("elecmag*")):
+        os.chdir(a)
+        os.system("pdflatex -shell-escape %s" % glob.glob("elecmag_*.tex")[0] )
+        os.chdir("..")
+elif sys.argv[1] == 'clean':
+    os.system("find . -name '_region_*' | xargs rm  -rf")
+    os.system("find . -name '_minted-*' | xargs rm  -rf")
+    os.system("find . -name '*.log' | xargs rm  -rf")
+    os.system("find . -name '*.aux' | xargs rm  -rf")
+    os.system("find . -name '*.out' | xargs rm  -rf")
+
+elif sys.argv[1] == 'tex':
+    file = glob.glob('elecmag_*.tex')
+    os.system("pdflatex -shell-escape %s" % file[0])
+    
+    
