1414ca5b6ff1088ade268f4d4df5540f308201a4
BB <me@test.com>
2019-02-05 14:25:57 +0300

    -

--- linear/linear_app88rbf/tst2.py
+++ linear/linear_app88rbf/tst2.py
@@ -1,7 +1,6 @@
 from mpl_toolkits.mplot3d import Axes3D
 import matplotlib.pyplot as plt
 from matplotlib import cm
-import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -13,17 +12,20 @@ def func(x, y):
     g2 = np.exp( -2 *np.log(2) * ((x-x2)**2+(y-y2)**2) / s2**2)
     return g1 + g2
 
-points = np.random.rand(1000, 2)
-
-x = np.linspace(36,37,100)
-y = np.linspace(32,33,100)
+D = 100
+x = np.linspace(36,37,D)
+y = np.linspace(32,33,D)
 
 xx,yy = np.meshgrid(x,y)
 zz = func(xx,yy)
 
-fig = plt.figure()
-ax = fig.gca(projection='3d')
-surf = ax.plot_surface(xx, yy, zz, cmap=cm.coolwarm,linewidth=0, antialiased=False)
-fig.colorbar(surf, shrink=0.5, aspect=5)
-plt.savefig('/data/data/com.termux/files/home/Downloads/out1.png')
+xxx = xx.reshape(D*D)
+yyy = yy.reshape(D*D)
+zzz = zz.reshape(D*D)
+idx = np.random.choice(range(D*D),300)
 
+xr = xxx[idx]
+yr = yyy[idx]
+zr = zzz[idx]
+
+print (xr[0],yr[0],zr[0])
