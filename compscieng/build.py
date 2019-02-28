f785be02308fa9ee97077b43129e256305233042
BB <me@test.com>
2019-02-28 11:52:50 +0300

    -

--- compscieng/build.py
+++ compscieng/build.py
@@ -1,4 +1,4 @@
-import os, sys, glob
+import os, sys, glob, shutil
 
 pdfs = " ".join(sorted(list(glob.glob('./*/*.pdf'))))
 
@@ -19,5 +19,9 @@ elif sys.argv[1] == 'clean':
 elif sys.argv[1] == 'tex':
     file = glob.glob('compscieng_*.tex')
     os.system("pdflatex -shell-escape %s" % file[0])
+    d = "/data/data/com.termux/files/home/storage/downloads"
+    if os.path.isdir(d):
+        ff = file[0].replace(".tex",".pdf")
+        shutil.copy(ff,d)
     
     
