3348726164f80209d85d611b68f8c8568a8c958f
Burak Bayramli <burak@localhost.com>
2019-07-15 01:18:58 +0300

    -

--- phy/phy_lander/test2.py
+++ phy/phy_lander/test2.py
@@ -1,5 +1,5 @@
-import matplotlib.animation as animation
 import gym, six
+import pyglet
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
@@ -8,10 +8,16 @@ env = gym.make('LunarLander-v2')
 
 observation = env.reset() 
 
-for t in range(100):
+for i in range(100):
     action = env.action_space.sample() 
     env.render()
     observation, reward, done, info = env.step(action)
+
+    buffer = pyglet.image.get_buffer_manager().get_color_buffer()            
+    image_data = buffer.get_image_data()
+    if i % 10 == 0:
+        image_data.save(filename='frames/out-%04d.png' % i)
+    
     print (observation)
     if done: break
     
