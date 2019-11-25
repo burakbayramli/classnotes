29f11e7b170bb0093d8fe031a0338f1917df8dbe
BB <burak@localhost.com>
2019-11-25 21:14:29 +0300

    -

--- func_analysis/func_70_dfo/test6.py
+++ func_analysis/func_70_dfo/test6.py
@@ -446,143 +446,6 @@ class LagrangianHessian(object):
 
 class VectorFunction(object):
 
-    def __init__(self, fun, x0, jac, hess,
-                 finite_diff_rel_step, finite_diff_jac_sparsity,
-                 finite_diff_bounds, sparse_jacobian):
-        if not callable(jac) and jac not in FD_METHODS:
-            raise ValueError("`jac` must be either callable or one of {}."
-                             .format(FD_METHODS))
-
-        if not (callable(hess) or hess in FD_METHODS
-                or isinstance(hess, HessianUpdateStrategy)):
-            raise ValueError("`hess` must be either callable,"
-                             "HessianUpdateStrategy or one of {}."
-                             .format(FD_METHODS))
-
-        if jac in FD_METHODS and hess in FD_METHODS:
-            raise ValueError("Whenever the Jacobian is estimated via "
-                             "finite-differences, we require the Hessian to "
-                             "be estimated using one of the quasi-Newton "
-                             "strategies.")
-
-        self.x = np.atleast_1d(x0).astype(float)
-        self.n = self.x.size
-        self.nfev = 0
-        self.njev = 0
-        self.nhev = 0
-        self.f_updated = False
-        self.J_updated = False
-        self.H_updated = False
-
-        finite_diff_options = {}
-        if jac in FD_METHODS:
-            finite_diff_options["method"] = jac
-            finite_diff_options["rel_step"] = finite_diff_rel_step
-            if finite_diff_jac_sparsity is not None:
-                sparsity_groups = group_columns(finite_diff_jac_sparsity)
-                finite_diff_options["sparsity"] = (finite_diff_jac_sparsity,
-                                                   sparsity_groups)
-            finite_diff_options["bounds"] = finite_diff_bounds
-            self.x_diff = np.copy(self.x)
-        if hess in FD_METHODS:
-            finite_diff_options["method"] = hess
-            finite_diff_options["rel_step"] = finite_diff_rel_step
-            finite_diff_options["as_linear_operator"] = True
-            self.x_diff = np.copy(self.x)
-        if jac in FD_METHODS and hess in FD_METHODS:
-            raise ValueError("Whenever the Jacobian is estimated via "
-                             "finite-differences, we require the Hessian to "
-                             "be estimated using one of the quasi-Newton "
-                             "strategies.")
-
-        # Function evaluation
-        def fun_wrapped(x):
-            self.nfev += 1
-            return np.atleast_1d(fun(x))
-
-        def update_fun():
-            self.f = fun_wrapped(self.x)
-
-        self._update_fun_impl = update_fun
-        update_fun()
-
-        self.v = np.zeros_like(self.f)
-        self.m = self.v.size
-
-        self._update_jac_impl = update_jac
-
-        # Define Hessian
-        if callable(hess):
-            self.H = hess(self.x, self.v)
-            self.H_updated = True
-            self.nhev += 1
-
-            if sps.issparse(self.H):
-                def hess_wrapped(x, v):
-                    self.nhev += 1
-                    return sps.csr_matrix(hess(x, v))
-                self.H = sps.csr_matrix(self.H)
-
-            elif isinstance(self.H, LinearOperator):
-                def hess_wrapped(x, v):
-                    self.nhev += 1
-                    return hess(x, v)
-
-            else:
-                def hess_wrapped(x, v):
-                    self.nhev += 1
-                    return np.atleast_2d(np.asarray(hess(x, v)))
-                self.H = np.atleast_2d(np.asarray(self.H))
-
-            def update_hess():
-                self.H = hess_wrapped(self.x, self.v)
-        elif hess in FD_METHODS:
-            def jac_dot_v(x, v):
-                return jac_wrapped(x).T.dot(v)
-
-            def update_hess():
-                self._update_jac()
-                self.H = approx_derivative(jac_dot_v, self.x,
-                                           f0=self.J.T.dot(self.v),
-                                           args=(self.v,),
-                                           **finite_diff_options)
-            update_hess()
-            self.H_updated = True
-        elif isinstance(hess, HessianUpdateStrategy):
-            self.H = hess
-            self.H.initialize(self.n, 'hess')
-            self.H_updated = True
-            self.x_prev = None
-            self.J_prev = None
-
-            def update_hess():
-                self._update_jac()
-                if self.x_prev is not None and self.J_prev is not None:
-                    delta_x = self.x - self.x_prev
-                    delta_g = self.J.T.dot(self.v) - self.J_prev.T.dot(self.v)
-                    self.H.update(delta_x, delta_g)
-
-        self._update_hess_impl = update_hess
-
-        if isinstance(hess, HessianUpdateStrategy):
-            def update_x(x):
-                self._update_jac()
-                self.x_prev = self.x
-                self.J_prev = self.J
-                self.x = np.atleast_1d(x).astype(float)
-                self.f_updated = False
-                self.J_updated = False
-                self.H_updated = False
-                self._update_hess()
-        else:
-            def update_x(x):
-                self.x = np.atleast_1d(x).astype(float)
-                self.f_updated = False
-                self.J_updated = False
-                self.H_updated = False
-
-        self._update_x_impl = update_x
-
     def _update_v(self, v):
         if not np.array_equal(v, self.v):
             self.v = v
@@ -685,13 +548,7 @@ class PreparedConstraint(object):
 
     def __init__(self, constraint, x0, sparse_jacobian=None,
                  finite_diff_bounds=(-np.inf, np.inf)):
-        if isinstance(constraint, NonlinearConstraint):
-            fun = VectorFunction(constraint.fun, x0,
-                                 constraint.jac, constraint.hess,
-                                 constraint.finite_diff_rel_step,
-                                 constraint.finite_diff_jac_sparsity,
-                                 finite_diff_bounds, sparse_jacobian)
-        elif isinstance(constraint, LinearConstraint):
+        if isinstance(constraint, LinearConstraint):
             fun = LinearVectorFunction(constraint.A, x0, sparse_jacobian)
         elif isinstance(constraint, Bounds):
             fun = IdentityVectorFunction(x0, sparse_jacobian)
@@ -1297,12 +1154,10 @@ def update_state_sqp(state, x, last_iteration_failed, objective, prepared_constr
     state.nfev = objective.nfev
     state.njev = objective.ngev
     state.nhev = objective.nhev
-    state.constr_nfev = [c.fun.nfev if isinstance(c.fun, VectorFunction) else 0
-                         for c in prepared_constraints]
-    state.constr_njev = [c.fun.njev if isinstance(c.fun, VectorFunction) else 0
-                         for c in prepared_constraints]
-    state.constr_nhev = [c.fun.nhev if isinstance(c.fun, VectorFunction) else 0
-                         for c in prepared_constraints]
+    
+    state.constr_nfev = [0 for c in prepared_constraints]
+    state.constr_njev = [0 for c in prepared_constraints]
+    state.constr_nhev = [0 for c in prepared_constraints]
 
     if not last_iteration_failed:
         state.x = x
@@ -1477,7 +1332,7 @@ def projected_cg(H, c, Z, Y, b, trust_radius=np.inf,
     if return_all:
         allvecs = [x]
     H_p = H.dot(p)
-    rt_g = norm(g)**2  # g.T g = r.T Z g = r.T g (ref [1]_ p.1389)
+    rt_g = norm(g)**2  
 
     tr_distance = trust_radius - norm(x)
     if tr_distance < 0:
