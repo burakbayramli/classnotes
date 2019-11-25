e9cf7497b8b756ddd06d0b8726c478036a8333a0
BB <burak@localhost.com>
2019-11-25 21:36:30 +0300

    -

--- func_analysis/func_70_dfo/test6.py
+++ func_analysis/func_70_dfo/test6.py
@@ -169,37 +169,6 @@ class CanonicalConstraint(object):
         return cls(0, 0, fun, jac, hess, np.empty(0, dtype=np.bool))
 
 
-    @classmethod
-    def _equal_to_canonical(cls, cfun, value):
-        empty_fun = np.empty(0)
-        n = cfun.n
-
-        n_eq = value.shape[0]
-        n_ineq = 0
-        keep_feasible = np.empty(0, dtype=bool)
-
-        if cfun.sparse_jacobian:
-            empty_jac = sps.csr_matrix((0, n))
-        else:
-            empty_jac = np.empty((0, n))
-
-        def fun(x):
-            return cfun.fun(x) - value, empty_fun
-
-        def jac(x):
-            return cfun.jac(x), empty_jac
-
-        def hess(x, v_eq, v_ineq):
-            return cfun.hess(x, v_eq)
-
-        empty_fun = np.empty(0)
-        n = cfun.n
-        if cfun.sparse_jacobian:
-            empty_jac = sps.csr_matrix((0, n))
-        else:
-            empty_jac = np.empty((0, n))
-
-        return cls(n_eq, n_ineq, fun, jac, hess, keep_feasible)
 
     @classmethod
     def _less_to_canonical(cls, cfun, ub, keep_feasible):
