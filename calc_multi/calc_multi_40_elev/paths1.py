88ea4e77be1ed655200ab57a4a330667d8d8c372
BB <burak@localhost.com>
2019-12-24 13:46:17 +0300

    -

--- calc_multi/calc_multi_40_elev/paths1.py
+++ calc_multi/calc_multi_40_elev/paths1.py
@@ -43,9 +43,13 @@ def calc_int(pars):
     ts = np.linspace(0,1.0,100)
     # for z 
     dzs = np.array([_approx_fprime_helper([t],gfunc)[0] for t in ts])
-    I1 = np.sqrt(1+dzs**2)
-    I = trapz(I1, 1/100.)
-    print (I)
+    tmp = 1 + np.sqrt(1+dzs**2)
+    Iv = trapz(tmp, 1/100.)
+    print (Iv)
+    tmp = np.array([np.sqrt(b1 + 2*b2*t + 3*b3*t**2 - 112.0*t**3 + (a1 + 2*a2*t + 3*a3*t**2 - 65.2*t**3)**2) for t in ts])
+    Ih = trapz(tmp, 1/100.)
+    return Iv*5 + Ih*1
+        
     
 def test_fprime():
     rosen = lambda x: (1-x[0])**2 + 100*(x[1]-x[0]**2)**2
@@ -57,12 +61,14 @@ def test_fprime():
     
 
 # 1. gidis yolunun tanimi, uzun yoldan dolanarak gidiyor
-a1,a2,a3 = 1.5, 8.1, 4.0
-b1,b2,b3 = 0.3, 0.4, 23.3
+#a1,a2,a3 = 1.5, 8.1, 4.0
+#b1,b2,b3 = 0.3, 0.4, 23.3
+a1,a2,a3 = 1.5, 3.0, 1.0
+b1,b2,b3 = 0.0, 1.0, 1.0
 a0,b0=(1.0,1.0)
 ex,ey=(0.3,4.0)
 a4 = ex - a0 - (a1+a2+a3)
 b4 = ey - b0 - (b1+b2+b3)
 test_coefs1 = (a0,a1,a2,a3,a4,b0,b1,b2,b3,b4)
-calc_int(test_coefs1)
+print (calc_int(test_coefs1))
 test_fprime()
