4cd47aab89e6f2dc02c9f8dc19c5567b9e90ce28
BB <burak@localhost.com>
2019-12-24 14:33:45 +0300

    -

--- calc_multi/calc_multi_40_elev/paths1.py
+++ calc_multi/calc_multi_40_elev/paths1.py
@@ -10,24 +10,25 @@ def trapz(y, dx):
     vals = vals[vals>0.0]
     return (y[0]+np.sum(vals*2.0)+y[-1])*(dx/2.0)
 
-def gfunc(x):
-    t = x[0]
-    x = a0 + a1*t + a2*t**2 + a3*t**3 + a4*t**4 
-    y = b0 + b1*t + b2*t**2 + b3*t**3 + b4*t**4
-    s1 = 2.2; x1 = 2.0; y1 = 2.0
-    g1 = np.exp( -4 *np.log(2) * ((x-x1)**2+(y-y1)**2) / s1**2)
-    return g1
-
 def calc_int(pars):
-    pars = a0,a1,a2,a3,a4,b0,b1,b2,b3,b4
+    a0,a1,a2,a3,a4,b0,b1,b2,b3,b4=pars
+    def gfunc(t):
+        t = t[0]
+        x = a0 + a1*t + a2*t**2 + a3*t**3 + a4*t**4 
+        y = b0 + b1*t + b2*t**2 + b3*t**3 + b4*t**4
+        s1 = 2.2; x1 = 2.0; y1 = 2.0
+        g1 = np.exp( -4 *np.log(2) * ((x-x1)**2+(y-y1)**2) / s1**2)
+        return g1*10.0
+    
     ts = np.linspace(0,1.0,100)
     # for z 
     dzs = np.array([util._approx_fprime_helper([t],gfunc)[0] for t in ts])
-    tmp = 1 + np.sqrt(1+dzs**2)
+    tmp = np.sqrt(1+(dzs**2))
     Iv = trapz(tmp, 1/100.)
-    print (Iv)
+    #print (Iv)
     tmp = np.array([b1 + 2*b2*t + 3*b3*t**2 - 112.0*t**3 + (a1 + 2*a2*t + 3*a3*t**2 - 65.2*t**3)**2 for t in ts])
     tmp = tmp[tmp>0.0]
+    print (tmp)
     tmp = np.sqrt(tmp)
     Ih = trapz(tmp, 1/100.)
     return Iv*5 + Ih*1
@@ -36,8 +37,10 @@ def calc_int(pars):
 # 1. gidis yolunun tanimi, uzun yoldan dolanarak gidiyor
 #a1,a2,a3 = 1.5, 8.1, 4.0
 #b1,b2,b3 = 0.3, 0.4, 23.3
-a1,a2,a3 = 1.5, 3.0, 1.0
-b1,b2,b3 = 0.0, 1.0, 1.0
+#a1,a2,a3 = 1.5, 3.0, 1.0
+#b1,b2,b3 = 0.0, 1.0, 1.0
+a1,a2,a3 = 0.5, 0.1, 0.0
+b1,b2,b3 = -0.3, -1.4, 1.3
 a0,b0=(1.0,1.0)
 ex,ey=(0.3,4.0)
 a4 = ex - a0 - (a1+a2+a3)
