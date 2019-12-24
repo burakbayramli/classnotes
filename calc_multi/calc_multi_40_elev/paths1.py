ab878f31a506538432d9151973e11d7319ad9862
BB <burak@localhost.com>
2019-12-24 15:32:22 +0300

    -

--- calc_multi/calc_multi_40_elev/paths1.py
+++ calc_multi/calc_multi_40_elev/paths1.py
@@ -3,7 +3,11 @@ import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 from scipy.spatial.distance import cdist
 from matplotlib import cm
+import numpy.linalg as lin
 import util
+
+a0,b0=(1.0,1.0)
+ex,ey=(0.3,4.0)
 
 def trapz(y, dx):
     vals = y[1:-1]
@@ -11,7 +15,9 @@ def trapz(y, dx):
     return (y[0]+np.sum(vals*2.0)+y[-1])*(dx/2.0)
 
 def calc_int(pars):
-    a0,a1,a2,a3,a4,b0,b1,b2,b3,b4=pars
+    a1,a2,a3,b1,b2,b3=pars
+    a4 = ex - a0 - (a1+a2+a3)
+    b4 = ey - b0 - (b1+b2+b3)
     def gfunc(t):
         t = t[0]
         x = a0 + a1*t + a2*t**2 + a3*t**3 + a4*t**4 
@@ -28,11 +34,14 @@ def calc_int(pars):
     #print (Iv)
     tmp = np.array([b1 + 2*b2*t + 3*b3*t**2 - 112.0*t**3 + (a1 + 2*a2*t + 3*a3*t**2 - 65.2*t**3)**2 for t in ts])
     tmp = tmp[tmp>0.0]
-    print (tmp)
     tmp = np.sqrt(tmp)
     Ih = trapz(tmp, 1/100.)
     return Iv*5 + Ih*1
-            
+
+def calc_int_approx(x):
+    g = _approx_fprime_helper(x, calc_int)
+    return calc_int(x),g
+
 
 # 1. gidis yolunun tanimi, uzun yoldan dolanarak gidiyor
 #a1,a2,a3 = 1.5, 8.1, 4.0
@@ -41,9 +50,11 @@ def calc_int(pars):
 #b1,b2,b3 = 0.0, 1.0, 1.0
 a1,a2,a3 = 0.5, 0.1, 0.0
 b1,b2,b3 = -0.3, -1.4, 1.3
-a0,b0=(1.0,1.0)
-ex,ey=(0.3,4.0)
 a4 = ex - a0 - (a1+a2+a3)
 b4 = ey - b0 - (b1+b2+b3)
-test_coefs1 = (a0,a1,a2,a3,a4,b0,b1,b2,b3,b4)
+test_coefs1 = (a1,a2,a3,b1,b2,b3)
 print (calc_int(test_coefs1))
+
+#bfgs([0,0,0,0,0,0],rosen_approx)
+
+
