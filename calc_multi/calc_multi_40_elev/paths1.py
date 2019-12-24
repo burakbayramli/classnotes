994fe662b94179314eeefc1a3bcd2f531ce3b43c
BB <burak@localhost.com>
2019-12-24 13:08:15 +0300

    -

--- calc_multi/calc_multi_40_elev/paths1.py
+++ calc_multi/calc_multi_40_elev/paths1.py
@@ -4,26 +4,55 @@ from mpl_toolkits.mplot3d import Axes3D
 from scipy.spatial.distance import cdist
 from matplotlib import cm
 
+eps = np.sqrt(np.finfo(float).eps)
+
+def _approx_fprime_helper(xk, f, epsilon):
+    f0 = f(xk)
+    grad = np.zeros((len(xk),), float)
+    ei = np.zeros((len(xk),), float)
+    for k in range(len(xk)):
+        ei[k] = 1.0
+        d = epsilon * ei
+        df = (f(xk + d) - f0) / d[k]
+        if not np.isscalar(df):
+            try:
+                df = df.item()
+            except (ValueError, AttributeError):
+                raise ValueError("The user-provided "
+                                 "objective function must "
+                                 "return a scalar value.")
+        grad[k] = df
+        ei[k] = 0.0
+    return grad
+
 def trapz(y, dx):
     vals = y[1:-1]
     vals = vals[vals>0.0]
     return (y[0]+np.sum(vals*2.0)+y[-1])*(dx/2.0)
 
-def gfunc(x, y):
+def gfunc(t):
+    x = a0 + a1*t + a2*t**2 + a3*t**3 + a4*t**4 
+    y = b0 + b1*t + b2*t**2 + b3*t**3 + b4*t**4
     s1 = 2.2; x1 = 2.0; y1 = 2.0
     g1 = np.exp( -4 *np.log(2) * ((x-x1)**2+(y-y1)**2) / s1**2)
-    return g1 
+    return g1
 
 def calc_int(pars):
     pars = a0,a1,a2,a3,a4,b0,b1,b2,b3,b4
     t = np.linspace(0,1.0,100)
-    x = a0 + a1*t + a2*t**2 + a3*t**3 + a4*t**4 
-    y = b0 + b1*t + b2*t**2 + b3*t**3 + b4*t**4
-
     # for z 
-    z = gfunc(x,y)
+    z = gfunc(t)
     print (z)
     
+def test_fprime():
+    rosen = lambda x: (1-x[0])**2 + 100*(x[1]-x[0]**2)**2
+
+    def rosen_d(x):
+        return np.array([2*100*(x[1] - x[0]**2)*(-2*x[0]) - 2*(1.-x[0]), 2*100*(x[1]-x[0]**2)])
+
+    x = [0.5, 0.5]
+    res = _approx_fprime_helper(x, rosen, eps)
+    print ('analitik', res, 'sayisal', rosen_d(x))    
     
 
 # 1. gidis yolunun tanimi, uzun yoldan dolanarak gidiyor
@@ -34,4 +63,5 @@ ex,ey=(0.3,4.0)
 a4 = ex - a0 - (a1+a2+a3)
 b4 = ey - b0 - (b1+b2+b3)
 test_coefs1 = (a0,a1,a2,a3,a4,b0,b1,b2,b3,b4)
-print (calc_int(test_coefs1))
+calc_int(test_coefs1)
+test_fprime()
