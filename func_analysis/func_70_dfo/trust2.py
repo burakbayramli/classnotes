7694818821d2c0c16b72e08b61472de9a82193a1
BB <burak@localhost.com>
2019-12-26 14:23:38 +0300

    -

--- func_analysis/func_70_dfo/trust2.py
+++ func_analysis/func_70_dfo/trust2.py
@@ -2273,52 +2273,6 @@ def reinforce_box_boundaries(x, lb, ub):
 
 
 def modified_dogleg(A, Y, b, trust_radius, lb, ub):
-    """Approximately  minimize ``1/2*|| A x + b ||^2`` inside trust-region.
-
-    Approximately solve the problem of minimizing ``1/2*|| A x + b ||^2``
-    subject to ``||x|| < Delta`` and ``lb <= x <= ub`` using a modification
-    of the classical dogleg approach.
-
-    Parameters
-    ----------
-    A : LinearOperator (or sparse matrix or ndarray), shape (m, n)
-        Matrix ``A`` in the minimization problem. It should have
-        dimension ``(m, n)`` such that ``m < n``.
-    Y : LinearOperator (or sparse matrix or ndarray), shape (n, m)
-        LinearOperator that apply the projection matrix
-        ``Q = A.T inv(A A.T)`` to the vector.  The obtained vector
-        ``y = Q x`` being the minimum norm solution of ``A y = x``.
-    b : array_like, shape (m,)
-        Vector ``b``in the minimization problem.
-    trust_radius: float
-        Trust radius to be considered. Delimits a sphere boundary
-        to the problem.
-    lb : array_like, shape (n,)
-        Lower bounds to each one of the components of ``x``.
-        It is expected that ``lb <= 0``, otherwise the algorithm
-        may fail. If ``lb[i] = -Inf`` the lower
-        bound for the i-th component is just ignored.
-    ub : array_like, shape (n, )
-        Upper bounds to each one of the components of ``x``.
-        It is expected that ``ub >= 0``, otherwise the algorithm
-        may fail. If ``ub[i] = Inf`` the upper bound for the i-th
-        component is just ignored.
-
-    Returns
-    -------
-    x : array_like, shape (n,)
-        Solution to the problem.
-
-    Notes
-    -----
-    Based on implementations described in p.p. 885-886 from [1]_.
-
-    References
-    ----------
-    .. [1] Byrd, Richard H., Mary E. Hribar, and Jorge Nocedal.
-           "An interior point algorithm for large-scale nonlinear
-           programming." SIAM Journal on Optimization 9.4 (1999): 877-900.
-    """
     # Compute minimum norm minimizer of 1/2*|| A x + b ||^2.
     newton_point = -Y.dot(b)
     # Check for interior point
@@ -2372,82 +2326,6 @@ def projected_cg(H, c, Z, Y, b, trust_radius=np.inf,
                  lb=None, ub=None, tol=None,
                  max_iter=None, max_infeasible_iter=None,
                  return_all=False):
-    """Solve EQP problem with projected CG method.
-
-    Solve equality-constrained quadratic programming problem
-    ``min 1/2 x.T H x + x.t c``  subject to ``A x + b = 0`` and,
-    possibly, to trust region constraints ``||x|| < trust_radius``
-    and box constraints ``lb <= x <= ub``.
-
-    Parameters
-    ----------
-    H : LinearOperator (or sparse matrix or ndarray), shape (n, n)
-        Operator for computing ``H v``.
-    c : array_like, shape (n,)
-        Gradient of the quadratic objective function.
-    Z : LinearOperator (or sparse matrix or ndarray), shape (n, n)
-        Operator for projecting ``x`` into the null space of A.
-    Y : LinearOperator,  sparse matrix, ndarray, shape (n, m)
-        Operator that, for a given a vector ``b``, compute smallest
-        norm solution of ``A x + b = 0``.
-    b : array_like, shape (m,)
-        Right-hand side of the constraint equation.
-    trust_radius : float, optional
-        Trust radius to be considered. By default uses ``trust_radius=inf``,
-        which means no trust radius at all.
-    lb : array_like, shape (n,), optional
-        Lower bounds to each one of the components of ``x``.
-        If ``lb[i] = -Inf`` the lower bound for the i-th
-        component is just ignored (default).
-    ub : array_like, shape (n, ), optional
-        Upper bounds to each one of the components of ``x``.
-        If ``ub[i] = Inf`` the upper bound for the i-th
-        component is just ignored (default).
-    tol : float, optional
-        Tolerance used to interrupt the algorithm.
-    max_iter : int, optional
-        Maximum algorithm iterations. Where ``max_inter <= n-m``.
-        By default uses ``max_iter = n-m``.
-    max_infeasible_iter : int, optional
-        Maximum infeasible (regarding box constraints) iterations the
-        algorithm is allowed to take.
-        By default uses ``max_infeasible_iter = n-m``.
-    return_all : bool, optional
-        When ``true`` return the list of all vectors through the iterations.
-
-    Returns
-    -------
-    x : array_like, shape (n,)
-        Solution of the EQP problem.
-    info : Dict
-        Dictionary containing the following:
-
-            - niter : Number of iterations.
-            - stop_cond : Reason for algorithm termination:
-                1. Iteration limit was reached;
-                2. Reached the trust-region boundary;
-                3. Negative curvature detected;
-                4. Tolerance was satisfied.
-            - allvecs : List containing all intermediary vectors (optional).
-            - hits_boundary : True if the proposed step is on the boundary
-              of the trust region.
-
-    Notes
-    -----
-    Implementation of Algorithm 6.2 on [1]_.
-
-    In the absence of spherical and box constraints, for sufficient
-    iterations, the method returns a truly optimal result.
-    In the presence of those constraints the value returned is only
-    a inexpensive approximation of the optimal value.
-
-    References
-    ----------
-    .. [1] Gould, Nicholas IM, Mary E. Hribar, and Jorge Nocedal.
-           "On the solution of equality constrained quadratic
-            programming problems arising in optimization."
-            SIAM Journal on Scientific Computing 23.4 (2001): 1376-1395.
-    """
     CLOSE_TO_ZERO = 1e-25
 
     n, = np.shape(c)  # Number of parameters
