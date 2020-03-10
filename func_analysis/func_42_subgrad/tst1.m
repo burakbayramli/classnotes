a9d5eeb35f9d1491d13c9a1a1e378160d4dedd2d
BB <burak@localhost.com>
2020-03-10 18:43:36 +0300

    -

---
+++ func_analysis/func_42_subgrad/tst1.m
@@ -0,0 +1,12 @@
+clear;clc;
+%% generate random synthetic data
+n = 30; p = 30; sparse_ratio = 0.01;
+A = randn(n,p);
+beta = randn(p,1);
+
+beta(randperm(p,round((1-sparse_ratio)*p)))=0;beta0 = randn;
+b = beta0 + A*beta + randn(n,1);
+
+lambda = 0.1;
+
+subgrad_func(A,b,lambda);
