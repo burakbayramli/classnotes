7cf99ac4a9ff11b1d4f5504f6b39be26e378a183
BB <burak@localhost.com>
2020-04-12 15:52:29 +0300

    -

--- stat/stat_nflix/run.sh
+++
@@ -1,3 +0,0 @@
-#python prepare.py /home/burak/Downloads/movielens2.csv A.dat # large
-#python prepare.py /home/burak/Downloads/movielens_SMALL/movielens2.csv A.dat # medium
-python prepare.py w1.csv A.dat # small
--- stat/stat_nflix/sasha/README.md
+++
@@ -1,6 +0,0 @@
-**Large Scale SVD on map/reduce**
-
-Arxiv paper http://arxiv.org/abs/1310.4664.
-
-You can install Sasha from https://github.com/burakbayramli/sasha
-
