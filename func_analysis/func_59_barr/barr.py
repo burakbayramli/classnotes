7553de2c8c9108af0e45d8e38c4b2264a6fb01f8
BB <burak@localhost.com>
2020-04-16 13:13:10 +0300

    -

--- func_analysis/func_59_barr/barr.py
+++ func_analysis/func_59_barr/barr.py
@@ -2,12 +2,34 @@
 import numpy as np
 
 class LPCenteringSolver():
+    """
+    A helper module for LPSolver
+
+    Solves LP centering problem using infeasible start Newton method:
+    minimize    c'x - sum( log(x_i) )
+    subject to  Ax = b
+
+    Hyperparameters
+        alpha: the factor by which we weaken the slope of first-order approximation
+               in backtracking line search (0 < alpha < 0.5)
+        beta: multiplicative step size in backtracking line search (0 < beta < 1)
+        max_iter: maximum number of Newton steps before quitting
+
+    Attributes
+        status: 'optimal' or None
+        value: optimal value of objective function
+        x_opt, v_opt: primal optimal point and a dual optimal point
+        num_steps: Number of Newton steps executed
+        residual_norm: array of ||r(x,v)||_2 (length num_steps+1)
+    """
+
     def __init__(self, alpha=0.25, beta=0.5, max_iter=100):
         self.alpha = alpha
         self.beta = beta
         self.max_iter = max_iter
         self.convergence_threshold = 1e-6  # stop when ||r||_2 <= convergence_threshold
         self.status = None
+
 
     def solve(self, A, b, c, x0=None):
         m, n = A.shape
@@ -62,6 +84,32 @@ class LPCenteringSolver():
 
 
 class LPSolver():
+    """
+    LPSolver solves the standard form LP problem:
+    minimize    c'x
+    subject to  Ax = b, x >= 0
+
+    The solver uses the barrier method.
+    It traverses the central path by solving a centering problem at each step:
+    minimize    t * c'x - sum( log(x_i) )
+    subject to  Ax = b
+
+    The solving procedure gradually increases t after each step, and stops when
+    the duality gap n / t < tol, where n is the length of x
+
+    Hyperparameters
+        mu: multiplicative step size for barrier method (t := mu * t)
+        tol: suboptimality tolerance (used for convergence check)
+
+    Attributes:
+        status: 'optimal', 'infeasible', or 'failure'
+        value: optimal value of objective function
+        x_opt, v_opt: primal optimal point and a dual optimal point
+        num_steps: number of centering steps executed
+        duality_gaps: array - duality gap after every centering step
+        newton_steps: array - number of Newton steps for solving centering problems
+    """
+
     def __init__(self, mu=10, tol=1e-3) :
         self.mu = mu  # multiplicative step size for barrier method
         self.tol = tol  # maximum acceptable duality gap
@@ -70,6 +118,11 @@ class LPSolver():
 
 
     def solve_feasible_start(self, A, b, c, x):
+        """
+        Solves an LP problem given a strictly feasible starting point x.
+        This is used as a helper method for solving an LP problem from scratch,
+        no matter whether the problem is strictly feasible or not
+        """
         m, n = A.shape
         status, x_opt, v_opt, value = None, None, None, None
         duality_gaps = []  # duality gap n/t after every iteration
@@ -78,7 +131,6 @@ class LPSolver():
         t = 1
         for num_steps in range(1, self.max_iter+1):
             # Solve LP Centering problem
-            print ('x',x)
             self.centering_solver.solve(A, b, t*c, x)
             if self.centering_solver.status != 'optimal':
                 status = 'failure'  # Centering solver failed
@@ -103,10 +155,24 @@ class LPSolver():
 
 
     def solve(self, A, b, c):
+        """
+        Checks feasibility of an LP problem, and solves the problem if it's feasible
+        The solving procedure consists of two phases:
+        -- Phase I: Check strict feasibility and obtain a strictly feasible starting
+                    point by using solve_feasible_start() to solve the following LP,
+                    for which we can always find a feasible starting point (x, t):
+                    minimize    t
+                    subject to  Ax = b, x >= (1 - t) * 1_n
+
+        -- Phase II: If strictly feasible, directly invoke solve_feasible_start()
+                     with the starting point x found in Phase I
+        """
         m, n = A.shape
 
+        # Phase I: Check strict feasibility and obtain a strictly feasible starting point
         x = np.linalg.pinv(A).dot(b)
         if np.min(x) <= 0:  # otherwise, x is already a strictly feasible point
+            # Construct an LP for checking feasibility
             A1 = np.hstack(( A, -A.dot(np.ones((n, 1))) ))
             b1 = b - A.dot(np.ones(n))
             c1 = np.zeros(n+1)
