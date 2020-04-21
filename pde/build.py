f216ddb25db6986043ee68401e655d01256ba6b4
BB <burak@localhost.com>
2020-04-21 14:06:33 +0300

    -

--- pde/build.py
+++ pde/build.py
@@ -1,21 +1,10 @@
 import os, sys, glob
+
+pdfs = " ".join(sorted(list(glob.glob('./*/*.pdf'))))
 d = os.environ['HOME']
-if len(sys.argv) == 1 :
-    os.system("pdfunite \
-    pde_00/pde_00.pdf \
-    pde_01/pde_01.pdf \
-    pde_02/pde_02.pdf \
-    pde_03/pde_03.pdf \
-    pde_04/pde_04.pdf \
-    pde_heat/pde_heat.pdf \
-    pde_heat_deriv/pde_heat_deriv.pdf \
-    pde_level/pde_level.pdf \
-    pde_lk/pde_lk.pdf \
-    pde_curvature/pde_curvature.pdf \
-    pde_wave_deriv/pde_wave_deriv.pdf \
-    pde_zapp/pde_zapp.pdf \
-    " + d + "/Downloads/pde.pdf" \
-    )
+if len(sys.argv) == 1:
+    cmd = "pdfunite %s " + d + "/Downloads/pde.pdf"
+    os.system(cmd % pdfs)
     exit()
     
 elif sys.argv[1] == 'all':
@@ -23,11 +12,12 @@ elif sys.argv[1] == 'all':
         os.chdir(a)
         os.system("pdflatex -shell-escape %s" % glob.glob("pde_*.tex")[0] )
         os.chdir("..")
-               
+        
+    
 elif sys.argv[1] == 'clean':
     os.system("find . -name '_region_*' | xargs rm  -rf")
     os.system("find . -name '_minted-*' | xargs rm  -rf")
-    
+            
 elif sys.argv[1] == 'tex':
     file = glob.glob('pde_*.tex')
     os.system("pdflatex -shell-escape %s" % file[0])
