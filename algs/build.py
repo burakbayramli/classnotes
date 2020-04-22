c275bcb874c357327a88eadef575c8c036439283
BB <burak@localhost.com>
2020-04-22 12:37:30 +0300

    -

--- algs/build.py
+++ algs/build.py
@@ -1,66 +1,26 @@
-import os, sys, glob, shutil
+import os, sys, glob
+
+pdfs = " ".join(sorted(list(glob.glob('./*/*.pdf'))))
 d = os.environ['HOME']
+
 if len(sys.argv) == 1:
-    cmd = "pdfunite ./cover/cover.pdf \
-    ./mlintro/mlintro.pdf \
-    ./complexity/complexity.pdf \
-    ./basic/basic.pdf \
-    ./sort/sort.pdf \
-    ./dynp/dynp.pdf  \
-    ./minspan/minspan.pdf \
-    ./dijks/dijks.pdf \
-    ./mstseg/mstseg.pdf \
-    ./probsolve/probsolve.pdf \
-    ./mcts/mcts.pdf \
-    ./go/go.pdf \
-    ./huff/huff.pdf \
-    ./id3/id3.pdf \
-    ./knn/knn.pdf \
-    ./enc/enc.pdf \
-    ./kmeans/kmeans.pdf \
-    ./kmeans_mr/kmeans_mr.pdf \
-    ./meanshift/meanshift.pdf \
-    ./modeval/modeval.pdf \
-    ./crf/crf.pdf \
-    ./mesquita/mesquita.pdf \
-    ./graph/graph.pdf \
-    ./rsa/rsa.pdf \
-    ./mlp/mlp.pdf \
-    ./autodiff/autodiff.pdf \
-    ./tensorflow/tensorflow.pdf \
-    ./convnet/convnet.pdf \
-    ./rnn/rnn.pdf \
-    ./lstm/lstm.pdf \
-    ./autoenc/autoenc.pdf \
-    ./reinf/reinf.pdf \
-    ./nlp/nlp.pdf \
-    ./recom/recom.pdf \
-    ./nmt/nmt.pdf \
-    ./ocr/ocr.pdf \
-    ./speech/speech.pdf \
-    ./graphconv/graphconv.pdf \
-    ./gan/gan.pdf \
-    ./zapp/zapp.pdf \
-    " + d + "/Downloads/algs.pdf"
+    cmd = "pdfunite " + pdfs + " " + d + "/Downloads/algs.pdf"
     os.system(cmd)
-    print (cmd)
-    exit()
+    exit()    
 elif sys.argv[1] == 'all':
-    print (os.listdir("."))
-    for a in os.listdir("."):
-        if os.path.isdir(a):
-            os.chdir(a)
-            os.system("pdflatex -shell-escape *.tex")    
-            os.chdir("..")
-               
+    for a in sorted(glob.glob("algs*")):
+        os.chdir(a)
+        os.system("pdflatex -shell-escape %s" % glob.glob("algs_*.tex")[0] )
+        os.chdir("..")
 elif sys.argv[1] == 'clean':
     os.system("find . -name '_region_*' | xargs rm  -rf")
+    os.system("find . -name '_minted-*' | xargs rm  -rf")
+    os.system("find . -name '*.log' | xargs rm  -rf")
+    os.system("find . -name '*.aux' | xargs rm  -rf")
+    os.system("find . -name '*.out' | xargs rm  -rf")
 
 elif sys.argv[1] == 'tex':
-    file = glob.glob('*.tex')
+    file = glob.glob('algs_*.tex')
     os.system("pdflatex -shell-escape %s" % file[0])
-    d = "/data/data/com.termux/files/home/storage/downloads"
-    if os.path.isdir(d):
-        ff = file[0].replace(".tex",".pdf")
-        shutil.copy(ff,d)
-        
+    
+    
