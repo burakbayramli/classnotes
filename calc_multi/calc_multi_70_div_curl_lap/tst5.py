111b51de11bfeaf8429f3055611eb9c78a55e3e8
BB <you@example.com>
2020-09-22 14:05:30 +0300

    -

---
+++ calc_multi/calc_multi_70_div_curl_lap/tst5.py
@@ -0,0 +1,37 @@
+import numpy as np
+import scipy.io as sio
+
+mat = sio.loadmat('/home/burak/Documents/classnotes/calc_multi/calc_multi_70_div_curl_lap/wind.mat')
+x = mat['x']; y = mat['y']; z = mat['z']
+u = mat['u']; v = mat['v']; w = mat['w']
+
+dx = x[0,:,0]
+dy = y[:,0,0]
+dz = z[0,0,:]
+
+dummy, dFx_dy, dFx_dz = np.gradient (u, dx, dy, dz, axis=[1,0,2])
+dFy_dx, dummy, dFy_dz = np.gradient (v, dx, dy, dz, axis=[1,0,2])
+dFz_dx, dFz_dy, dummy = np.gradient (w, dx, dy, dz, axis=[1,0,2])
+
+rot_x = dFz_dy - dFy_dz
+rot_y = dFx_dz - dFz_dx
+rot_z = dFy_dx - dFx_dy
+
+l = np.sqrt(np.power(u,2.0) + np.power(v,2.0) + np.power(w,2.0));
+
+print (l.shape)
+
+m1 = np.multiply(rot_x,u)
+m2 = np.multiply(rot_y,v)
+m3 = np.multiply(rot_z,w)
+
+tmp1 = (m1 + m2 + m3)
+tmp2 = np.multiply(l,2.0)
+
+av = np.divide(tmp1, tmp2)
+
+i=2;j=2;k=2;
+print (rot_x[i,j,k])
+print (rot_y[i,j,k])
+print (rot_z[i,j,k])
+
