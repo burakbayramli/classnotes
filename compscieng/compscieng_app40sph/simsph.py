9bd3d7f0752b3730554ac4eeef01696ab4d900cb
User1 <mk@yomama.com>
2020-10-28 14:38:00 +0300

    -

--- compscieng/compscieng_app40sph/simsph.py
+++ compscieng/compscieng_app40sph/simsph.py
@@ -55,8 +55,8 @@ class Simulation:
                 for zs in np.linspace(0.0, 0.2, 4):
                     v = np.array([0.0, 0.0, 0.0])
                     f = 5*np.array([0,0,0])
-                    p = np.array([xs, ys, zs])
-                    self.balls.append({'pos':p, 'f':f, 'v': v, 'i': bi})
+                    x = np.array([xs, ys, zs])
+                    self.balls.append({'x': x, 'f':f, 'v': v, 'i': bi})
                     bi += 1
 
                         
@@ -87,40 +87,40 @@ class Simulation:
         
         for j,b in enumerate(self.balls):
             b['v'] += self.dt*(b['f']/m)
-            b['pos'] += self.dt*b['v']
+            b['x'] += self.dt*b['v']
             
-            if (abs(b['pos'][0]) >= self.mmax):
+            if (abs(b['x'][0]) >= self.mmax):
                 #print (b['i'], 'wall 1')
                 b['v'][0] *= -self.cor
-                if b['pos'][0] < 0:
-                    b['pos'][0] = self.mmin
+                if b['x'][0] < 0:
+                    b['x'][0] = self.mmin
 
-            if (abs(b['pos'][1]) >= self.mmax):
+            if (abs(b['x'][1]) >= self.mmax):
                 #print (b['i'], 'wall 2')
                 b['v'][1] *= -self.cor
-                if b['pos'][1] < 0:
-                    b['pos'][1] = self.mmin
+                if b['x'][1] < 0:
+                    b['x'][1] = self.mmin
                     
-            if (abs(b['pos'][2]) >= self.mmax):
+            if (abs(b['x'][2]) >= self.mmax):
                 #print (b['i'], 'wall 3')
                 b['v'][2] *= -self.cor
-                if b['pos'][2] < 0:
-                    b['pos'][2] = self.mmin
+                if b['x'][2] < 0:
+                    b['x'][2] = self.mmin
 
         for j,b in enumerate(self.balls):
-            self.geo_hash_list[spatial_hash(self.balls[j]['pos'])].append(self.balls[j])
+            self.geo_hash_list[spatial_hash(self.balls[j]['x'])].append(self.balls[j])
 
         vDone = {}
         for j,b in enumerate(self.balls):
-            if (len(self.geo_hash_list[spatial_hash(self.balls[j]['pos'])])>1):
-                otherList = self.geo_hash_list[spatial_hash(self.balls[j]['pos'])]
+            if (len(self.geo_hash_list[spatial_hash(self.balls[j]['x'])])>1):
+                otherList = self.geo_hash_list[spatial_hash(self.balls[j]['x'])]
                 for other in otherList:
                     if (other['i'] != b['i'] and b['i'] not in vDone and other['i'] not in vDone):
-                        dist = lin.norm(other['pos']-b['pos'])
+                        dist = lin.norm(other['x']-b['x'])
                         if (dist < (2*self.r)):
                             #print ('collision')
                             vrel = b['v']-other['v']
-                            n = (other['pos']-b['pos']) / dist
+                            n = (other['x']-b['x']) / dist
                             vnorm = np.dot(vrel,n)*n
                             #print (vnorm)
                             b['v'] = b['v'] - vnorm
@@ -150,7 +150,7 @@ class Simulation:
         glutWireCube(2.0)
         for j,b in enumerate(self.balls):
             glPushMatrix()
-            glTranslatef(b['pos'][0],b['pos'][1],b['pos'][2])
+            glTranslatef(b['x'][0],b['x'][1],b['x'][2])
             glMaterialfv(GL_FRONT, GL_DIFFUSE, [0.0, 0.0, 1.0, 1.0])
             glutSolidSphere(self.r,50,50)
             glPopMatrix()
