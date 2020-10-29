e25860e00bdb6b2687c3596839dd8ef693bc53c8
User1 <mk@yomama.com>
2020-10-29 14:18:25 +0300

    -

--- compscieng/compscieng_app40sph/simsph2.py
+++ compscieng/compscieng_app40sph/simsph2.py
@@ -10,18 +10,18 @@ import numpy as np, datetime
 import sys, numpy.linalg as lin
 
 p1,p2,p3 = 73856093, 19349663, 83492791
-#G = np.array([0.0, 0.0, -0.8])
-G = np.array([0.0, 0.0, -0.8])
+#G = np.array([0.0, 0.0, -9.8*12000.0])
+G = np.array([0.0, 0.0, -9.8])
 
 B = 10 # top
 l = 0.2 # bolec kutu buyuklugu
 n = B*20 # bolec sozluk buyuklugu
 
-GAS_CONST = 0.1
-REST_DENS = 0.1
+REST_DENS = 1000.0
+GAS_CONST = 2000.0
 MASS = 65.0
-VISC = 1.0
-DT = 0.1
+VISC = 250.0
+DT = 0.0008
 H = 0.2 # kernel radius
 HSQ = H*H # radius^2 for optimization
 POLY6 = 315.0/(65.0*np.pi*np.power(H, 9.));
@@ -80,15 +80,15 @@ class Simulation:
 
     def hash_balls(self):
         self.geo_hash_list = defaultdict(list)        
-        for j,b in enumerate(self.balls):
-            self.geo_hash_list[spatial_hash(self.balls[j]['x'])].append(self.balls[j])
+        for i,b in enumerate(self.balls):
+            self.geo_hash_list[spatial_hash(self.balls[i]['x'])].append(self.balls[i])
           
     def computeDensityPressure(self):
-        for i,bi in enumerate(self.balls):
+        for i,bi in enumerate(self.balls):            
+            bi['rho'] = 0.0                
             h = spatial_hash(self.balls[i]['x']) # su anki topun boleci
             if (len(self.geo_hash_list[h])>1): # yakinda top var mi
                 otherList = self.geo_hash_list[h] # varsa isle
-                bi['rho'] = 0.0                
                 for j,bj in enumerate(otherList):
                     r2 = lin.norm(bi['x']-bj['x'])**2
                     if  r2 < HSQ:
@@ -119,7 +119,8 @@ class Simulation:
     def integrate(self):
         
         for j,b in enumerate(self.balls):
-            b['v'] += self.dt*(b['f']/MASS)
+            if b['rho'] > 0.0: 
+                b['v'] += self.dt*(b['f']/b['rho'])
             b['x'] += self.dt*b['v']
             
             if (abs(b['x'][0]) >= self.mmax):
