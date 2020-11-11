f53e5845558c71c7281bf566b81fb852afb90b7e
User1 <mk@yomama.com>
2020-11-11 13:54:37 +0300

    -

--- algs/algs_070_knn/CompactNSearch/make.sh
+++
@@ -1,2 +0,0 @@
-g++  -c CompactNSearch.cpp -o /tmp/CompactNSearch.o  -g -Wall -O2 
-g++ main.cpp -g -Wall -O2 -o /tmp/a.exe  -fopenmp -lstdc++ -lm /tmp/CompactNSearch.o 
--- algs/algs_070_knn/CompactNSearch/morton_BMI.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-
-#if defined(__BMI2__)
-
-#endif
