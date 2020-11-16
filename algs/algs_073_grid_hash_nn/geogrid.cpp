7cda8a71ccc4d5c44fce7e628c2bcfb3e0ea4537
User1 <mk@yomama.com>
2020-11-16 11:38:20 +0300

    -

--- algs/algs_073_grid_hash_nn/geogrid.cpp
+++ algs/algs_073_grid_hash_nn/geogrid.cpp
@@ -1,7 +1,6 @@
-// How to compile, and other info
-// https://burakbayramli.github.io/dersblog/sk/2020/08/sph.html
+//
 // g++ geogrid.cpp -g -Wall -O2 -o /tmp/a.exe; /tmp/a.exe
-// https://github.com/cerrno/mueller-sph
+//
 #include <iostream>
 #include <fstream> 
 #include <vector>
@@ -11,20 +10,15 @@ using namespace std;
 #include <eigen3/Eigen/Dense>
 using namespace Eigen;
 
-// "Particle-Based Fluid Simulation for Interactive Applications"
-// solver parameters
-const static Vector3d G(0.f, 0.f, 5.0f*-9.8f); // external (gravitational) forces
+const static Vector3d G(0.f, 0.f, 0.f); 
 
-// particle data structure
-// stores position, velocity, and force for integration
-// stores density (rho) and pressure values for SPH
 struct Particle {
     Particle(float _x, float _y, float _z) : x(_x, _y, _z) {}
     Vector3d x;
 };
 
-// solver data
 static vector<Particle> particles;
+
 static int B = 40;
 
 void InitSPH(void)
@@ -33,6 +27,7 @@ void InitSPH(void)
 	float x = rand() % 200;
 	float y = rand() % 200;
 	float z = rand() % 200;
+	std::cout << x << " " << y << " " << z<< std::endl;
 	particles.push_back(Particle(x,y,z));
     }
     std::cout << particles.size() << std::endl;
