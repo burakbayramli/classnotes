ce2851affeae260d80184e74a7103bb2e6bcd762
User1 <mk@yomama.com>
2020-11-16 15:10:26 +0300

    -

--- algs/algs_073_grid_hash_nn/geogrid.cpp
+++ algs/algs_073_grid_hash_nn/geogrid.cpp
@@ -13,7 +13,7 @@ using namespace std;
 #include <eigen3/Eigen/Dense>
 using namespace Eigen;
 
-static int B = 100;
+static int B = 3000;
 static int BIN_WIDTH = 10.f;
 static int BIN_NUM = 20;
 static int COORD_MAX = 200;
@@ -24,30 +24,44 @@ struct int3{
     int i, j, k;
 };
 
+
+/*
 struct int3compare
 {
    bool operator() (const int3& lhs, const int3& rhs) const
    {
-       return lhs.i < rhs.i && lhs.j < rhs.j && lhs.k < rhs.k;
+       return (lhs.i != rhs.i) || (lhs.j != rhs.j) || (lhs.k != rhs.k);
    }
 };
+*/
+
+bool operator<(int3 const& lhs, int3 const& rhs)
+{  
+   return lhs.i < rhs.i || lhs.j < rhs.j ||
+				   lhs.k < rhs.k ;
+}
+
+static int calcBin(float x) {
+    int res = (int)(x / BIN_WIDTH) + 1;
+    if (res >= BIN_NUM) return BIN_NUM;
+    return res;
+}
 
 struct Particle {
     Particle() : x(0.f,0.f,0.f) {}
-    Particle(float _x, float _y, float _z) : x(_x, _y, _z) {}
+    Particle(float _x, float _y, float _z) : x(_x, _y, _z) {
+	bin.i = calcBin(x[0]);
+	bin.j = calcBin(x[1]);
+	bin.k = calcBin(x[2]);
+    }
     Vector3d x;
     int3 bin;
 };
 
 static vector<Particle> particles;
 
-static int bin(float x) {
-    int res = (int)(x / BIN_WIDTH) + 1;
-    if (res >= BIN_NUM) return BIN_NUM;
-    return res;
-}
-
-std::map<int3,  std::vector<Particle>, int3compare> grid_hash; 
+//std::map<int3,  std::vector<Particle>, int3compare> grid_hash; 
+std::map<int3,  std::vector<Particle>> grid_hash; 
 
 void InitSPH(void)
 {
@@ -56,12 +70,11 @@ void InitSPH(void)
 	float y = rand() % COORD_MAX;
 	float z = rand() % COORD_MAX;
 	Particle p(x,y,z);
-	p.bin.i = bin(p.x[0]);
-	p.bin.j = bin(p.x[1]);
-	p.bin.k = bin(p.x[2]);
 
-	std::cout        
+	std::cout
+	    << i << " " 
 	    << "[" << p.x.transpose() << "]"
+	    <<  " in grid " 
 	    << " " << p.bin.i
 	    << " " << p.bin.j
 	    << " " << p.bin.k
@@ -88,7 +101,7 @@ std::vector<Particle> getNeighbors(Particle particle){
 		int nj = particle.bin.j + j;
 		int nk = particle.bin.k + k;
 		int3 newk(ni,nj,nk);
-		std::cout << "size " << ni << " " << ni << " " << nk 
+		std::cout << "neigh " << ni << " " << ni << " " << nk << " "
 			  << grid_hash[newk].size() << std::endl;
 	    }
 	}
@@ -100,12 +113,17 @@ std::vector<Particle> getNeighbors(Particle particle){
 
 int main(int argc, char** argv)
 {
+    srand (0);
     
     InitSPH();
 
-    std::cout << "neighbors " << particles[10].x << std::endl;
-    
-    std::vector<Particle> res = getNeighbors(particles[60]);
+    int idx = 111;
+    std::cout << "neighbors of " << particles[idx].x << std::endl;
+
+    std::cout << "at " << particles[idx].bin.i << " " << particles[idx].bin.j << " " << particles[idx].bin.k << " "
+	      << std::endl;
+
+    std::vector<Particle> res = getNeighbors(particles[idx]);
     
     
     return 0;
