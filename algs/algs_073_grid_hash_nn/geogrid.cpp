112321830baacac062b8bde1e92556e9bcbdaca8
User1 <mk@yomama.com>
2020-11-16 14:00:54 +0300

    -

--- algs/algs_073_grid_hash_nn/geogrid.cpp
+++ algs/algs_073_grid_hash_nn/geogrid.cpp
@@ -16,14 +16,23 @@ using namespace Eigen;
 const static Vector3d G(0.f, 0.f, 0.f);
 
 struct int3{
+    int3() {} 
+    int3(int _i, int _j, int _k) { i=_i; j=_j; k=_k;}
     int i, j, k;
+};
+
+struct int3compare
+{
+   bool operator() (const int3& lhs, const int3& rhs) const
+   {
+       return lhs.i < rhs.i && lhs.j < rhs.j && lhs.k < rhs.k;
+   }
 };
 
 struct Particle {
     Particle() : x(0.f,0.f,0.f) {}
     Particle(float _x, float _y, float _z) : x(_x, _y, _z) {}
     Vector3d x;
-    //Vector3d bin;
     int3 bin;
 };
 
@@ -41,8 +50,7 @@ static int bin(float x) {
     return res;
 }
 
-//std::map<Vector3d,  std::vector<Particle>> m_bins; 
-std::map<int3,  std::vector<Particle>> m_bins; 
+std::map<int3,  std::vector<Particle>, int3compare> grid_hash; 
 
 void InitSPH(void)
 {
@@ -54,10 +62,16 @@ void InitSPH(void)
 	p.bin.i = bin(p.x[0]);
 	p.bin.j = bin(p.x[1]);
 	p.bin.k = bin(p.x[2]);
+
 	std::cout        
 	    << "[" << p.x.transpose() << "]"
 	    << " " << p.bin.i << std::endl;
+	
 	particles.push_back(Particle(x,y,z));
+
+	int3 k(p.bin.i, p.bin.j, p.bin.k);
+	grid_hash[k].push_back(p);
+	
     }
     std::cout << particles.size() << std::endl;
 }
@@ -66,7 +80,15 @@ void InitSPH(void)
 
 int main(int argc, char** argv)
 {
-    InitSPH();
 
+    InitSPH();
+   
+    int3 i1(3,4,4);
+    int3 i2(3,49,4);
+
+    //grid_hash[i1] = particles;
+
+    std::cout << grid_hash[i1].size() << std::endl;
+    
     return 0;
 }
