f1c2884df3349a6eef04811d8c27b25f039b52fd
MK <me@yomama.com>
2021-07-06 14:09:34 +0300

    -

--- phy/phy_008_sim_rigbod/t3.py
+++ phy/phy_008_sim_rigbod/t3.py
@@ -7,7 +7,7 @@ prop = your_mesh.get_mass_properties()
 
 Ibody = np.round(prop[2],3)
 Ibodyinv = lin.inv(Ibody)
-dt = 0.01
+dt = 0.1
 x = np.zeros((3,1))
 R = np.eye(3,3)
 L = np.zeros((3,1))
@@ -22,18 +22,65 @@ def skew(a):
 tidx = 2000
 apply_at = np.mean(your_mesh.vectors[tidx],axis=0)
 f_at = -your_mesh.get_unit_normals()[tidx]
-tau0 = np.cross(apply_at, f_at).reshape(3,1)
+tau0 = np.cross(apply_at, f_at).reshape(3,1) * 10.0
 
-for i in range(3):
-   print ('iteration=',i)
+res = []
+for i in range(30):
    xold,Rold,Pold,Lold = x.copy(),R.copy(),P.copy(),L.copy()
-   v = Pold / M
-   x = x + v*dt
+   
    Iinv = np.dot(np.dot(Rold, Ibodyinv), Rold.T)
    omega = np.dot(Iinv, Lold)
    skew_omega = skew(omega.reshape(3))
    R = Rold + np.dot(skew_omega, Rold) * dt
+   
+   v = Pold / M
+   x = x + v*dt
    #F = ...
+   P = Pold
    if i==0:
       L = Lold + tau0*dt
+      print ('first L',L)
+   else:      
+      L = Lold
+   res.append([x,R,P,L])
    print (R)
+
+#exit()   
+
+import matplotlib.pyplot as plt
+from mpl_toolkits import mplot3d
+
+LIM = 5
+
+def plot_vector(fig, orig, v, color='blue'):
+   ax = fig.gca(projection='3d')
+   orig = np.array(orig); v=np.array(v)
+   ax.quiver(orig[0], orig[1], orig[2], v[0], v[1], v[2],color=color)
+   ax = fig.gca(projection='3d')  
+   return fig
+
+SCALE = 4
+
+
+for i, [x,R,P,L] in enumerate(res):
+   print (R)
+   fig = plt.figure()
+   axes = mplot3d.Axes3D(fig)
+   your_mesh = mesh.Mesh.from_file('torus.stl')
+   # force application
+   o = np.mean(your_mesh.vectors[tidx],axis=0)
+   n = your_mesh.get_unit_normals()[tidx]
+   plot_vector(fig, o, -n*SCALE, color='red')
+   
+   your_mesh.rotate_using_matrix(R)
+   scale = your_mesh.points.flatten()
+   axes.add_collection3d(mplot3d.art3d.Poly3DCollection(your_mesh.vectors,alpha=0.3))
+   plot_vector(fig, [0,0,0], omega, color='red')
+   axes.auto_scale_xyz(scale, scale, scale)
+   axes.set_xlim(-LIM,LIM);axes.set_ylim(-LIM,LIM);axes.set_zlim(-LIM,LIM)
+   
+   axes.view_init(azim=84,elev=28)
+   plt.savefig('/tmp/rotate_%02d.png' % i)
+   plt.close('all') 
+   
+# convert -delay 20 -loop 0 /tmp/rotate*.png /tmp/rotate1.gif
