2517971b2d2cc475d9026b27f6c0caf4fbaf74a8
BB <me@yomama.com>
2021-12-10 20:02:45 +0300

    -

--- compscieng/compscieng_1_15/rls.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-
-def rlse_online(aT_k1,b_k1,x,P): 
-    K = np.dot(P,aT_k1.T)/(np.dot(np.dot(aT_k1,P),aT_k1.T)+1)
-    x = x +K*(b_k1-np.dot(aT_k1,x))
-    P = P-np.dot(K,np.dot(aT_k1,P))
-    return x,K,P
-
