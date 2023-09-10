b9a521cc67e2249630804ab8ec06025d4c2026fd
BB <me@yomama.com>
2023-09-11 01:30:51 +0300

    -

--- invidx.py
+++ invidx.py
@@ -3,6 +3,7 @@ from collections import defaultdict
 from unidecode import unidecode
 
 WORD = re.compile(r'\w+')
+target_dir = "/home/burak/Documents/repos/burakbayramli.github.com"    
 
 def clean_text(text):
     text = text.replace("\n"," ").replace("\r"," ")
@@ -22,11 +23,11 @@ def index_dir():
 
     invidx = defaultdict(lambda: defaultdict(int))
     dir = os.getcwd()
-    files1 = glob.glob(dir + "/**/**/*.md")
+    files1 = glob.glob(dir + "/**/**/**/*.md")
     files2 = glob.glob(dir + "/**/**/*.tex")
     files = files1 + files2
     files = sorted(files)
-    for file in enumerate(files):
+    for file in enumerate(files):        
         doc = file[1].replace(dir,"")    
         for word in reg_tokenize(open(file[1]).read()):
             word = unidecode(word).lower()
@@ -51,8 +52,6 @@ def index_dir():
         if first_letter in string.ascii_lowercase:
             invidx_dict[first_letter][k] = v
 
-    target_dir = "/home/burak/Documents/repos/burakbayramli.github.com"
-    
     for k,v in invidx_dict.items():
         fout = open(target_dir + "/idx/invidx-%s.json" % k,"w")
         fout.write(json.dumps(v))
@@ -62,9 +61,32 @@ def test1():
     res = util.get_title_from_tex('algs/algs_045_probsolve/algs_045_probsolve.tex')
     print (res)
     print (util.filename_from_title(res))
-        
+
+
+def test2():
+    search = "green teorisi"
+    #search = "convnet"
+    stok = search.split()
+    stok_hits = {}
+    results = []
+    for tok in stok:
+        stok_hits[tok] = json.loads(open(target_dir + "/idx/invidx-%s.json" % tok[0]).read())[tok]
+        results.append(set(stok_hits[tok]))
+
+    u = set.intersection(*results)
+
+    hits = []
+    for f in u:
+        hits.append([f,sum([stok_hits[tok][f] for tok in stok])])
+    
+    sorted_hits = sorted(range(len(hits)), key=lambda x: hits[x][1], reverse=True)
+    N = min(20,len(hits))
+    for i in range(N):
+        print (hits[sorted_hits[i]][0])
+    
+    
 if __name__ == "__main__": 
 
-    index_dir()
-    #test1()
+    #index_dir()
+    test2()
 
