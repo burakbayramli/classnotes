6f680666796c5091c5d452d6ebf0e7384a90b419
BB <me@yomama.com>
2025-07-24 13:53:50 +0300

    -

--- phy/phy_005_basics_06/tst2.py
+++ phy/phy_005_basics_06/tst2.py
@@ -34,8 +34,10 @@ S1,S2,N = 0,5,20
 dt = (S2-S1) / 20
 q = euclid.Quaternion(1,0,0,0)
 Jbodyinv = lin.inv(mesh.get_mass_properties()[2])
+F_ext  = f1-f0
+tau_ext = -np.cross(f1-cog,f1-f0)
 
-for i,t in enumerate(np.linspace(0,1,20)):
+for i,t in enumerate(np.linspace(S1,S2,N)):
     print ('----------------')
     fig, ax = plt.subplots(1, 1, subplot_kw={'projection': '3d'})
     # x ilk degeri cog, x degistikce cog'den ne kadar uzaklasmissa
@@ -43,35 +45,32 @@ for i,t in enumerate(np.linspace(0,1,20)):
     # o kadar yerinden oynatabiliriz.
     
     R = q.get_rotation_matrix_3x3().to_numpy_array()
-    #new_mesh  = mesh.vectors + (x - cog)
-    #new_mesh.rotate_using_matrix(R)
     currmesh = copy.deepcopy(mesh)
+    currmesh.vectors = currmesh.vectors + (x - cog)
     currmesh.rotate_using_matrix(R)
-    #print ('type',type(currmesh.vectors + (x - cog)))
-    obj = mplot3d.art3d.Poly3DCollection(currmesh.vectors + (x - cog))
+    obj = mplot3d.art3d.Poly3DCollection(currmesh.vectors)
     obj.set_edgecolor('k')
     obj.set_alpha(0.3)
     ax.add_collection3d(obj)
     plot_vector2(ax, f0, f1)
     plot_vector1(ax, f0, flin, color='cyan')
+    plot_vector1(ax, cog, tau_ext / 10, color='cyan')
     print (t)
     if t==0:
        # baslangicta p sifir, ve ilk F entegre edilerek ilk p
        # elde ediliyor. Ayni sekilde L.
-       F_ext  = f1-f0
        p = F_ext*dt
-       tau_ext = np.cross(f1-cog,f1-f0)
-       L = tau_ext*dt*100
+       L = tau_ext*dt*20
     x = x + dt*(p / m)
     print (x)
     R = q.get_rotation_matrix_3x3().to_numpy_array()
     Jinv = R.dot(Jbodyinv).dot(R.transpose())
     w = L.dot(Jinv)
-    wq = euclid.Quaternion(w=0, x=w[0], y=w[1], z=w[2])
+    wq = euclid.Quaternion(0, w[0], w[1], w[2])
     qdiff = (wq * q).scalar_mul(1/2).scalar_mul(dt)
     q = q.add(qdiff).normalize()
     print ('q new',q)        
     ax.set_xlim(30,70);ax.set_ylim(-10,30); ax.set_zlim(-10,30)
-    ax.view_init(elev=20, azim=200)    
+    ax.view_init(elev=20, azim=50)
     plt.savefig('img/out-%02d.jpg' % i)
     plt.close(fig)
--- phy/phy_073_rot/euclid.py
+++ phy/phy_073_rot/euclid.py
@@ -2544,3 +2544,43 @@ class Plane:
 #p = Point3(1.0, 2.0, 3.0)
 #print(s, p)
 #print(s.intersect(p))
+
+def quaternion_rotation_matrix(Q):
+    """
+    Covert a quaternion into a full three-dimensional rotation matrix.
+ 
+    Input
+    :param Q: A 4 element array representing the quaternion (q0,q1,q2,q3) 
+ 
+    Output
+    :return: A 3x3 element matrix representing the full 3D rotation matrix. 
+             This rotation matrix converts a point in the local reference 
+             frame to a point in the global reference frame.
+    """
+    # Extract the values from Q
+    q0 = Q.w
+    q1 = Q.x
+    q2 = Q.y
+    q3 = Q.z
+     
+    # First row of the rotation matrix
+    r00 = 2 * (q0 * q0 + q1 * q1) - 1
+    r01 = 2 * (q1 * q2 - q0 * q3)
+    r02 = 2 * (q1 * q3 + q0 * q2)
+     
+    # Second row of the rotation matrix
+    r10 = 2 * (q1 * q2 + q0 * q3)
+    r11 = 2 * (q0 * q0 + q2 * q2) - 1
+    r12 = 2 * (q2 * q3 - q0 * q1)
+     
+    # Third row of the rotation matrix
+    r20 = 2 * (q1 * q3 - q0 * q2)
+    r21 = 2 * (q2 * q3 + q0 * q1)
+    r22 = 2 * (q0 * q0 + q3 * q3) - 1
+     
+    # 3x3 rotation matrix
+    rot_matrix = np.array([[r00, r01, r02],
+                           [r10, r11, r12],
+                           [r20, r21, r22]])
+                            
+    return rot_matrix
